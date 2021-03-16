9b4a208fd4260a3c6029e2ceaae1385762aec2a1
Jordan Byrd <jabyrd3@gmail.com>
2021-03-16 16:26:35 -0400

    adds a dev compose file to get metrics shipping infux is for demo purposes only, need to get vectors remote_write prometheus sink configured

--- build/package/orb-agent/scripts/run-pktvisord.sh
+++ build/package/orb-agent/scripts/run-pktvisord.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-[[ "$VIZERD_ARGS" == "" ]] && VIZERD_ARGS="--full-api"
-
+[[ "$VIZERD_ARGS" == "" ]] && VIZERD_ARGS="--admin-api"
+/usr/local/bin/pktvisor_prometheus &
 exec pktvisord $VIZERD_ARGS
