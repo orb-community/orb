125ef4fda0443fec500303460deee109f267c6ec
dscabral <dcabral@daitan.com>
2021-08-02 15:20:58 -0300

    feat(fleet-management):adjust agent group struct as open api specification

--- fleet/agent_groups.go
+++ fleet/agent_groups.go
@@ -11,12 +11,14 @@ import (
 )
 
 type AgentGroup struct {
-	ID          string
-	Name        types.Identifier
-	MFOwnerID   string
-	MFChannelID string
-	Tags        types.Tags
-	Created     time.Time
+	ID             string
+	MFOwnerID      string
+	Name           types.Identifier
+	Description    string
+	MFChannelID    string
+	MatchingAgents types.Metadata
+	Tags           types.Tags
+	Created        time.Time
 }
 
 type AgentGroupService interface {
