0bf08cfbf6b992404fbe4a13ec4cda74485024b3
dscabral <dcabral@daitan.com>
2021-08-03 16:27:36 -0300

    test(agent_group)agent group creation unit tests

--- fleet/api/endpoint_test.go
+++ fleet/api/endpoint_test.go
@@ -1,3 +1,11 @@
+// Copyright (c) Mainflux
+// SPDX-License-Identifier: Apache-2.0
+
+// Adapted for Orb project, modifications licensed under MPL v. 2.0:
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 package api_test
 
 import (
