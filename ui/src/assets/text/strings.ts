6a3a5186739106ee683b6d3486b1633d73134fb3
gpazuch <1490938+gpazuch@users.noreply.github.com>
2021-08-10 09:24:33 -0300

    feat(ui-sinks-add): add sink strings
    Sink Management strings added to {STRINGS} for convenience.
    Initial structure could change to a more maleable configuration.
    Some strings should be added in the future.
    
    Add strings objects for sink pages

--- ui/src/assets/text/strings.ts
+++ ui/src/assets/text/strings.ts
@@ -8,25 +8,80 @@ export const STRINGS = {
   home: {
     title: 'Orb Observation Overview',
   },
+  // Fleet Pages strings
   fleet: {
     title: 'Fleet Management',
   },
+  // Sink Pages strings
   sink: {
-    header: 'All Sinks',
+    // sink statuses
+    status: {
+      active: 'Active',
+      error: 'Error',
+    },
+    // sink.interface name descriptors
+    propNames: {
+      id: 'id',
+      name: 'Name',
+      description: 'Description',
+      tags: 'Tags',
+      status: 'Status',
+      error: 'Error',
+      backend: 'Service Type',
+      config: 'Connection Details',
+      config_remote_host: 'Remote Host',
+      config_username: 'Username',
+      config_password: 'Password',
+      ts_created: 'Date Created',
+    },
+    // add page
+    add: {
+      header: 'Create New Sink',
+    },
+    // edit page
+    edit: {
+      header: 'Update Sink',
+    },
+    // delete modal
     delete: {
       header: 'Delete Sink Confirmation',
       body: 'Are you sure you want to delete this sink? This may cause policies which use this sink to become invalid. This action cannot be undone.',
       warning: '*To confirm, type your sink label exactly as it appears',
       close: 'Close',
     },
+    // details modal
     details: {
       header: 'Sink Details',
       close: 'Close',
-      name: 'Sink Name',
-      description: 'Sink Description',
-      backend: 'Sink Service Type',
-      remote_host: 'Remote Host',
-      ts_created: 'Date Created',
     },
+    // dashboard page
+    list: {
+      header: 'All Sinks',
+      none: 'There are no sinks listed.',
+      sink: 'sink',
+      total: ['You have', 'total.'],
+      error: 'have errors.',
+      create: 'New Sink',
+      filters: {
+        select: 'Filter',
+        name: 'Name',
+        description: 'Description',
+        status: 'Status',
+        type: 'Type',
+        tags: 'Tags',
+      },
+    },
+  },
+  // stepper cues
+  stepper: {
+    back: 'Back',
+    next: 'Next',
+    save: 'Save',
+  },
+  // tags cues
+  tags: {
+    addTag: 'Add New Tag',
+    key: 'Tag Key',
+    value: 'Tag Value',
   },
 };
