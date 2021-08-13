cef2e05a97a1594db3e72da0c8410a8cfb133945
Rovaris <lrovaris@daitan.com>
2021-08-13 02:48:10 -0300

    feature(agents-group-add): wip

--- ui/src/app/pages/agents/add/agent.add.component.ts
+++ ui/src/app/pages/agents/add/agent.add.component.ts
@@ -8,34 +8,6 @@ import {STRINGS} from 'assets/text/strings';
 import {sinkTypesList} from 'app/pages/sinks/sinks.component';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-
-const SETTINGS_EXAMPLE = {
-  prometheus: [
-    {
-      type: 'text',
-      input: 'text',
-      title: 'Remote Host',
-      name: 'remote_host',
-      required: true,
-    },
-    {
-      type: 'text',
-      input: 'text',
-      title: 'Username',
-      name: 'username',
-      required: true,
-    },
-    {
-      type: 'password',
-      input: 'text',
-      title: 'Password',
-      name: 'password',
-      required: true,
-    },
-  ],
-};
-
-
 @Component({
   selector: 'ngx-agent-add-component',
   templateUrl: './agent.add.component.html',
@@ -47,7 +19,7 @@ export class AgentAddComponent implements OnInit {
   secondFormGroup: FormGroup;
   isEditable = false;
 
-  strings = STRINGS;
+  strings = STRINGS.agents;
 
   customSinkSettings: {};
   selectedSinkSetting: any[];
@@ -82,27 +54,12 @@ export class AgentAddComponent implements OnInit {
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
-      name: ['my-prom-sink-', Validators.required],
+      name: ['', Validators.required],
       description: [''],
-      backend: ['', Validators.required],
     });
 
     this.secondFormGroup = this._formBuilder.group({});
 
-    /***TODO THIS IS JUST AN EXAMPLE OF HOW TO MAP WHAT COMES FROM THE BE TO SOMETHING THAT MAKES MORE
-     * SENSE IN THE FRONTEND.
-     */
-
-    this.customSinkSettings = Object.keys(SETTINGS_EXAMPLE).reduce((accumulator, curr) => {
-      accumulator[curr] = SETTINGS_EXAMPLE[curr].map(entry => ({
-        type: entry.type,
-        label: entry.title,
-        prop: entry.name,
-        input: entry.input,
-        required: entry.required,
-      }));
-      return accumulator;
-    }, {});
   }
 
   goBack() {
@@ -110,33 +67,25 @@ export class AgentAddComponent implements OnInit {
   }
 
   onFormSubmit() {
-    const payload = {
-      name: this.firstFormGroup.controls.name.value,
-      backend: this.firstFormGroup.controls.backend.value,
-      description: this.firstFormGroup.controls.description.value,
-      config: this.selectedSinkSetting.reduce((accumulator, current) => {
-        accumulator[current.prop] = this.secondFormGroup.controls[current.prop].value;
-        return accumulator;
-      }, {}),
-      tags: {
-        cloud: 'aws',
-      },
-      validate_only: false, // Apparently this guy is required..
-    };
-    // TODO Check this out
-    // console.log(payload);
-    this.sinksService.addSink(payload).subscribe(resp => {
-      this.notificationsService.success('Sink successfully created', '');
-      this.goBack();
-    });
+    // const payload = {
+    //   name: this.firstFormGroup.controls.name.value,
+    //   backend: this.firstFormGroup.controls.backend.value,
+    //   description: this.firstFormGroup.controls.description.value,
+    //   config: this.selectedSinkSetting.reduce((accumulator, current) => {
+    //     accumulator[current.prop] = this.secondFormGroup.controls[current.prop].value;
+    //     return accumulator;
+    //   }, {}),
+    //   tags: {
+    //     cloud: 'aws',
+    //   },
+    //   validate_only: false, // Apparently this guy is required..
+    // };
+    // // TODO Check this out
+    // // console.log(payload);
+    // this.sinksService.addSink(payload).subscribe(resp => {
+    //   this.notificationsService.success('Sink successfully created', '');
+    //   this.goBack();
+    // });
   }
 
-  onSinkTypeSelected(selectedValue) {
-    this.selectedSinkSetting = this.customSinkSettings[selectedValue];
-    const dynamicFormControls = this.selectedSinkSetting.reduce((accumulator, curr) => {
-      accumulator[curr.prop] = ['', curr.required ? Validators.required : null];
-      return accumulator;
-    }, {});
-    this.secondFormGroup = this._formBuilder.group(dynamicFormControls);
-  }
 }
