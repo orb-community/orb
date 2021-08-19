c7bade6efcc7db6fb17f053621c748f4eafcf008
gpazuch <1490938+gpazuch@users.noreply.github.com>
2021-08-19 12:19:13 -0300

    refactor(docs style orb-ui)!: Refactor Orb entities interfaces
    BREAKING CHANGES:
    	* refactor(orb-ui#agents) `agent.interface.ts`: add/remove fields as required.
    	* refactor(orb-ui#agents) `agent.group.interface.ts`: add/remove fields as required.
    
    CHANGES:
    	* refactor(orb-ui#sinks) `sink.interface.ts`: accept `|any` where typed `{[propName:string]:string}`
    	* docs(orb-ui#interfaces) Add compdoc compatible jsdoc strings to ORB interfaces
    	* style(orb-ui#interfaces) Whitespaces, newlines and such. code-style config file soon.
    
    Refactor Orb entities' model definitions

--- ui/src/app/common/interfaces/orb/agent.interface.ts
+++ ui/src/app/common/interfaces/orb/agent.interface.ts
@@ -1,9 +1,66 @@
+/**
+ * Agent Data Model Interface
+ *
+ * [Fleet Architecture]{@link https://github.com/ns1labs/orb/wiki/Architecture:-Fleet}
+ */
+
+/**
+ * @interface Agent
+ */
 export interface Agent {
-  /** id: UUIDv4 (read only) */
+  /**
+   *  ID {string} UUIDv4 (read only)
+   */
   id?: string;
-  key?: string;
-  channel_id?: string;
+
+  /**
+   * Name {string} [a-zA-Z_:][a-zA-Z0-9_]*
+   */
   name?: string;
+
+  /**
+   * A timestamp of creation {string}
+   */
+  ts_created?: string;
+
+  /**
+   * Channel ID {string}
+   * Comm. Ch. ID
+   * Unique to this agent
+   */
+  channel_id?: string;
+
+  /**
+   * Agent Tags {{[propName: string]: string}}
+   * Sent in by the agent when it connects
+   */
+  agent_tags?: any;
+
+  /**
+   * Orb Tags {{[propName: string]: string}}
+   * User defined tags
+   */
+  orb_tags?: any;
+
+  /**
+   * Agent Metadata {{[propName: string]: string}}
+   * Sent in by agent, defining its capabilities.
+   */
+  agent_metadata?: any;
+
+  /**
+   * State {string} = 'new'|'online'|'offline'|'stale'|'removed'
+   * Current Status of the Agent's Connection
+   */
   state?: string;
-  state?: string;
+
+  /**
+   * Last Heartbeat Data {{[propName: string]: string}}
+   */
+  last_hb_data?: any;
+
+  /**
+   * Last Heartbeat timestamp {string}
+   */
+  ts_lst_hb?: string;
 }
