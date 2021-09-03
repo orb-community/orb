34396dc4ccc88c5146dcaf472ad54f40786dd10d
gpazuch <1490938+gpazuch@users.noreply.github.com>
2021-09-03 13:33:50 -0300

    feat(orb-ui analytics): Add Analytics Tracking for router/page events
    Add tracking capability for router events.
    
    * Add `angular-google-tag-manager@^1.4.2` dependecy
    * Create new environment for `beta` builds
    * Create new Angular build config for `beta` builds `ng build --configuration beta --aot`
    * Create new package.json build target: `npm run build:prod:beta`
    * Add own test key to default environment for testing.
    * Add 'orb.beta.live' GTM key to beta env.
    
    Initial configuration and test environment for GTM.

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -4,7 +4,8 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 import { Component, OnInit } from '@angular/core';
-import { AnalyticsService } from './@core/utils/analytics.service';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-app',
@@ -12,10 +13,28 @@ import { AnalyticsService } from './@core/utils/analytics.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private analytics: AnalyticsService) {
+  constructor(private router: Router,
+              private gtmService: GoogleTagManagerService) {
   }
 
   ngOnInit() {
-    this.analytics.trackPageViews();
+    // I've commented about this on issue [#209](https://github.com/ns1labs/orb/issues/209)
+    // this.analytics.trackPageViews();
+    /**
+     * Track pages with router events
+     * Lets test this and think of other events to track down the road
+     * angular-google-tag-manager module might still be dropped out
+     * in favor of analyticsService that came bundled with mainfluxUI codebase
+     */
+    this.router.events.forEach(item => {
+      if (item instanceof NavigationEnd) {
+        const gtmTag = {
+          event: 'page',
+          pageName: item.url,
+        };
+
+        this.gtmService.pushTag(gtmTag);
+      }
+    });
   }
 }
