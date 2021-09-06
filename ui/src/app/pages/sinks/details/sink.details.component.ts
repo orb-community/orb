36c9004fa31451a84bebb4a3c661b6ede46992a5
Lucas Terranova Barberio <lbarberio@daitan.com>
2021-09-06 16:03:08 -0300

    fix orb ui files

--- ui/src/app/pages/sinks/details/sink.details.component.ts
+++ ui/src/app/pages/sinks/details/sink.details.component.ts
@@ -4,21 +4,13 @@ import { STRINGS } from 'assets/text/strings';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Sink } from 'app/common/interfaces/orb/sink.interface';
 
-const strings = STRINGS.sink;
-
 @Component({
   selector: 'ngx-sink-details-component',
   templateUrl: './sink.details.component.html',
   styleUrls: ['./sink.details.component.scss'],
 })
 export class SinkDetailsComponent {
-  header = strings.details.header;
-  close = strings.details.close;
-  name = strings.propNames.name;
-  description = strings.propNames.description;
-  backend = strings.propNames.backend;
-  remote_host = strings.propNames.config_remote_host;
-  ts_created = strings.propNames.ts_created;
+  strings = STRINGS.sink;
 
   @Input() sink: Sink = {};
 
@@ -27,16 +19,14 @@ export class SinkDetailsComponent {
     protected route: ActivatedRoute,
     protected router: Router,
   ) {
+    !this.sink.tags ? this.sink.tags = {} : null;
   }
 
   onOpenEdit(sink: any) {
-    this.router.navigate(
-      [`../sink/edit/${sink.id}`, sink.id], {
-        relativeTo: this.route,
-      });
+    this.dialogRef.close(true);
   }
 
   onClose() {
-    this.dialogRef.close();
+    this.dialogRef.close(false);
   }
 }
