6048ed869c055e7d74e5cac5e41e629efcee8f95
Igor Melo <imelo@daitan.com>
2021-09-10 16:34:30 -0300

    fix: fix minor ui issues

--- ui/src/app/pages/register/register.component.ts
+++ ui/src/app/pages/register/register.component.ts
@@ -1,15 +1,17 @@
-import { Component, ChangeDetectorRef, Inject } from '@angular/core';
+import { Component, ChangeDetectorRef, Inject, OnInit } from '@angular/core';
 
 import { NbAuthService, NB_AUTH_OPTIONS, NbRegisterComponent } from '@nebular/auth';
 import { Router } from '@angular/router';
+import { STRINGS } from 'assets/text/strings';
 
 @Component({
   selector: 'ngx-register-component',
   templateUrl: 'register.component.html',
+  styleUrls: ['register.component.scss'],
 })
-export class RegisterComponent extends NbRegisterComponent {
-  // user inherited
-  // strategy inherited
+export class RegisterComponent extends NbRegisterComponent implements OnInit {
+  strings = STRINGS.login;
+  showPassword = false;
 
   constructor(
     @Inject(NB_AUTH_OPTIONS) protected options: {},
@@ -18,6 +20,24 @@ export class RegisterComponent extends NbRegisterComponent {
     protected router: Router,
   ) {
     super(authService, options, cd, router);
+  }
+
+  ngOnInit() { // In the ngOnInit() or in the constructor
+    const el = document.getElementById('nb-global-spinner');
+    if (el) {
+      el.style['display'] = 'none';
+    }
+  }
+
+  getInputType() {
+    if (this.showPassword) {
+      return 'text';
+    }
+    return 'password';
+  }
+
+  toggleShowPassword() {
+    this.showPassword = !this.showPassword;
   }
 
   register() {
