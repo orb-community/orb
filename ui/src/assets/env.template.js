c79daf28877be8eac4814e76fbeaa0dc96f0a45d
gpazuch <1490938+gpazuch@users.noreply.github.com>
2021-09-20 16:11:10 -0300

    removing ui src folder

--- ui/src/assets/env.template.js
+++
@@ -1,6 +0,0 @@
-(function(window) {
-    window["env"] = window["env"] || {};
-  
-    // Environment variables
-    window["env"]["mqttWsUrl"] = "${MF_UI_MQTT_WS_URL}";
-  })(this);
--- ui/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-import { environment as defaultEnvironment } from './environment.defaults';
-
-export const environment = {
-  production: false,
-  GTAGID: 'G-387CGPZQF0',
-  ...defaultEnvironment,
-};
