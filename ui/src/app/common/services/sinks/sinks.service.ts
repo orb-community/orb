066c748742411aa8c8c80d450efa3a425d48d6ac
gpazuch <1490938+gpazuch@users.noreply.github.com>
2021-09-24 13:15:52 -0300

    fix(orb-ui): start fixing datatables refresh on edit, create, delete
    Test removing from cache, since there is 500ms debounce for retrieval of such lists.

--- ui/src/app/common/services/sinks/sinks.service.ts
+++ ui/src/app/common/services/sinks/sinks.service.ts
@@ -15,166 +15,168 @@ const defDir = 'desc';
 
 @Injectable()
 export class SinksService {
-    paginationCache: any = {};
-    cache: OrbPagination<Sink>;
+  paginationCache: any = {};
 
-    constructor(
-        private http: HttpClient,
-        private notificationsService: NotificationsService,
-    ) {
-        this.clean();
+  cache: OrbPagination<Sink>;
+
+  constructor(
+    private http: HttpClient,
+    private notificationsService: NotificationsService,
+  ) {
+    this.clean();
+  }
+
+  public static getDefaultPagination(): OrbPagination<Sink> {
+    return {
+      limit: defLimit,
+      order: defOrder,
+      dir: defDir,
+      offset: 0,
+      total: 0,
+      data: null,
+    };
+  }
+
+  clean() {
+    this.cache = {
+      limit: defLimit,
+      offset: 0,
+      order: defOrder,
+      total: 0,
+      dir: defDir,
+      data: [],
+    };
+    this.paginationCache = {};
+  }
+
+  addSink(sinkItem: Sink) {
+    return this.http.post(environment.sinksUrl,
+        sinkItem,
+        { observe: 'response' })
+      .map(
+        resp => {
+          return resp;
+        },
+      )
+      .catch(
+        err => {
+          this.notificationsService.error('Failed to create Sink',
+            `Error: ${ err.status } - ${ err.statusText } - ${ err.error.error }`);
+          return Observable.throwError(err);
+        },
+      );
+  }
+
+  getSinkById(sinkId: string): any {
+    return this.http.get(`${ environment.sinksUrl }/${ sinkId }`)
+      .map(
+        resp => {
+          return resp;
+        },
+      )
+      .catch(
+        err => {
+          this.notificationsService.error('Failed to fetch Sink',
+            `Error: ${ err.status } - ${ err.statusText }`);
+          return Observable.throwError(err);
+        },
+      );
+  }
+
+  getSinkBackends() {
+    return this.http.get(environment.sinkBackends)
+      .map((resp: any) => {
+        return resp.backends;
+      }).catch(
+        err => {
+          this.notificationsService.error('Failed to get Sink Backends',
+            `Error: ${ err.status } - ${ err.statusText }`);
+          return Observable.throwError(err);
+        },
+      );
+  }
+
+  getSinks(pageInfo: NgxDatabalePageInfo, isFilter = false) {
+    const offset = pageInfo.offset || this.cache.offset;
+    const limit = pageInfo.limit || this.cache.limit;
+    let params = new HttpParams()
+      .set('offset', (offset * limit).toString())
+      .set('limit', limit.toString())
+      .set('order', this.cache.order)
+      .set('dir', this.cache.dir);
+
+    if (isFilter) {
+      if (pageInfo.name) {
+        params = params.append('name', pageInfo.name);
+      }
+      if (pageInfo.tags) {
+        params.append('tags', JSON.stringify(pageInfo.tags));
+      }
+      this.paginationCache[offset] = false;
     }
 
-    public static getDefaultPagination(): OrbPagination<Sink> {
-        return {
-            limit: defLimit,
-            order: defOrder,
-            dir: defDir,
-            offset: 0,
-            total: 0,
-            data: null,
-        };
+    if (this.paginationCache[pageInfo.offset]) {
+      return of(this.cache);
     }
 
-    clean() {
-        this.cache = {
-            limit: defLimit,
-            offset: 0,
-            order: defOrder,
-            total: 0,
-            dir: defDir,
-            data: [],
-        };
-        this.paginationCache = {};
-    }
+    return this.http.get(environment.sinksUrl, { params })
+      .map(
+        (resp: any) => {
+          this.paginationCache[pageInfo.offset] = true;
+          // This is the position to insert the new data
+          const start = pageInfo.offset;
+          const newData = [...this.cache.data];
+          newData.splice(start, resp.limit, ...resp.sinks);
+          this.cache = {
+            ...this.cache,
+            offset: Math.floor(resp.offset / resp.limit),
+            total: resp.total,
+            data: newData,
+          };
+          if (pageInfo.name) this.cache.name = pageInfo.name;
+          if (pageInfo.tags) this.cache.tags = pageInfo.tags;
+          return this.cache;
+        },
+      )
+      .catch(
+        err => {
+          this.notificationsService.error('Failed to get Sinks',
+            `Error: ${ err.status } - ${ err.statusText }`);
+          return Observable.throwError(err);
+        },
+      );
+  }
 
-    addSink(sinkItem: Sink) {
-        return this.http.post(environment.sinksUrl,
-            sinkItem,
-            { observe: 'response' })
-            .map(
-                resp => {
-                    return resp;
-                },
-            )
-            .catch(
-                err => {
-                    this.notificationsService.error('Failed to create Sink',
-                        `Error: ${ err.status } - ${ err.statusText } - ${ err.error.error }`);
-                    return Observable.throwError(err);
-                },
-            );
-    }
+  editSink(sinkItem: Sink): any {
+    return this.http.put(`${ environment.sinksUrl }/${ sinkItem.id }`, sinkItem)
+      .map(
+        resp => {
+          return resp;
+        },
+      )
+      .catch(
+        err => {
+          this.notificationsService.error('Failed to edit Sink',
+            `Error: ${ err.status } - ${ err.statusText }`);
+          return Observable.throwError(err);
+        },
+      );
+  }
 
-    getSinkById(sinkId: string): any {
-        return this.http.get(`${ environment.sinksUrl }/${ sinkId }`)
-            .map(
-                resp => {
-                    return resp;
-                },
-            )
-            .catch(
-                err => {
-                    this.notificationsService.error('Failed to fetch Sink',
-                        `Error: ${ err.status } - ${ err.statusText }`);
-                    return Observable.throwError(err);
-                },
-            );
-    }
-
-    getSinkBackends() {
-        return this.http.get(environment.sinkBackends)
-            .map((resp: any) => {
-                return resp.backends;
-            }).catch(
-                err => {
-                    this.notificationsService.error('Failed to get Sink Backends',
-                        `Error: ${ err.status } - ${ err.statusText }`);
-                    return Observable.throwError(err);
-                },
-            );
-    }
-
-    getSinks(pageInfo: NgxDatabalePageInfo, isFilter = false) {
-        const offset = pageInfo.offset || this.cache.offset;
-        const limit = pageInfo.limit || this.cache.limit;
-        let params = new HttpParams()
-            .set('offset', (offset * limit).toString())
-            .set('limit', limit.toString())
-            .set('order', this.cache.order)
-            .set('dir', this.cache.dir);
-
-        if (isFilter) {
-            if (pageInfo.name) {
-                params = params.append('name', pageInfo.name);
-            }
-            if (pageInfo.tags) {
-                params.append('tags', JSON.stringify(pageInfo.tags));
-            }
-            this.paginationCache[offset] = false;
-        }
-
-        if (this.paginationCache[pageInfo.offset]) {
-            return of(this.cache);
-        }
-
-        return this.http.get(environment.sinksUrl, { params })
-            .map(
-                (resp: any) => {
-                    this.paginationCache[pageInfo.offset] = true;
-                    // This is the position to insert the new data
-                    const start = pageInfo.offset;
-                    const newData = [...this.cache.data];
-                    newData.splice(start, resp.limit, ...resp.sinks);
-                    this.cache = {
-                        ...this.cache,
-                        offset: Math.floor(resp.offset / resp.limit),
-                        total: resp.total,
-                        data: newData,
-                    };
-                    if (pageInfo.name) this.cache.name = pageInfo.name;
-                    if (pageInfo.tags) this.cache.tags = pageInfo.tags;
-                    return this.cache;
-                },
-            )
-            .catch(
-                err => {
-                    this.notificationsService.error('Failed to get Sinks',
-                        `Error: ${ err.status } - ${ err.statusText }`);
-                    return Observable.throwError(err);
-                },
-            );
-    }
-
-    editSink(sinkItem: Sink): any {
-        return this.http.put(`${ environment.sinksUrl }/${ sinkItem.id }`, sinkItem)
-            .map(
-                resp => {
-                    return resp;
-                },
-            )
-            .catch(
-                err => {
-                    this.notificationsService.error('Failed to edit Sink',
-                        `Error: ${ err.status } - ${ err.statusText }`);
-                    return Observable.throwError(err);
-                },
-            );
-    }
-
-    deleteSink(sinkId: string) {
-        return this.http.delete(`${ environment.sinksUrl }/${ sinkId }`)
-            .map(
-                resp => {
-                    return resp;
-                },
-            )
-            .catch(
-                err => {
-                    this.notificationsService.error('Failed to delete Sink',
-                        `Error: ${ err.status } - ${ err.statusText }`);
-                    return Observable.throwError(err);
-                },
-            );
-    }
+  deleteSink(sinkId: string) {
+    return this.http.delete(`${ environment.sinksUrl }/${ sinkId }`)
+      .map(
+        resp => {
+          this.cache.data.splice(this.cache.data.map(s=>s.id).indexOf(sinkId), 1);
+          return resp;
+        },
+      )
+      .catch(
+        err => {
+          this.notificationsService.error('Failed to delete Sink',
+            `Error: ${ err.status } - ${ err.statusText }`);
+          return Observable.throwError(err);
+        },
+      );
+  }
 }
