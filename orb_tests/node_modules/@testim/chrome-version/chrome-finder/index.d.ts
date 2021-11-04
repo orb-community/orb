5af779d71ff366a831a216b56cc13589159d2049
Igor Melo <imelo@daitan.com>
2021-11-04 08:13:57 -0300

    fix: branch refactor

--- orb_tests/node_modules/@testim/chrome-version/chrome-finder/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * find a executable chrome for all support system
- * @returns {string} executable chrome full path
- * @throws
- * if no executable chrome find, ERROR_NO_INSTALLATIONS_FOUND will be throw
- * if platform is not one if ['win32','darwin','linux'], ERROR_PLATFORM_NOT_SUPPORT will be throw
- */
-declare function findChrome(): string;
-export = findChrome;
