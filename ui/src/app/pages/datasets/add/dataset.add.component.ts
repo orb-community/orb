e84fcf8a66d7276378b6847bf4d6e55cdfd6f47d
gpazuch <1490938+gpazuch@users.noreply.github.com>
2021-11-05 18:06:36 -0300

    fix(orb-ui): dataset config available retrieval\n selected sinks shown by name, not id nor id:name

--- ui/src/app/pages/datasets/add/dataset.add.component.ts
+++ ui/src/app/pages/datasets/add/dataset.add.component.ts
@@ -13,13 +13,20 @@ import { AgentGroup } from 'app/common/interfaces/orb/agent.group.interface';
 import { AgentPolicy } from 'app/common/interfaces/orb/agent.policy.interface';
 import { Sink } from 'app/common/interfaces/orb/sink.interface';
 
+const CONFIG = {
+  SINKS: 'SINKS',
+  GROUPS: 'GROUPS',
+  POLICIES: 'POLICIES',
+  DATASET: 'DATASET',
+};
+
 @Component({
   selector: 'ngx-dataset-add-component',
   templateUrl: './dataset.add.component.html',
   styleUrls: ['./dataset.add.component.scss'],
 })
 export class DatasetAddComponent {
-  // stepper vars
+  // stepper form groups
   detailsFormGroup: FormGroup;
 
   agentFormGroup: FormGroup;
@@ -28,23 +35,27 @@ export class DatasetAddComponent {
 
   sinkFormGroup: FormGroup;
 
-  dataset: Dataset;
-
-  datasetID: string;
-
-  availableAgentGroups = [];
-
-  availableAgentPolicies = [];
-
-  availableSinks = [];
-
-  selectedSinks = [];
+  // stores user selected sinks
+  selectedSinks: { id: string, name?: string }[] = [];
 
   isEdit: boolean;
 
-  isLoading = false;
+  // #load controls
+  isLoading = Object.entries(CONFIG)
+    .reduce((acc, [value]) => {
+      acc[value] = false;
+      return acc;
+    }, {});
 
-  datasetLoading = false;
+  datasetID: string;
+
+  dataset: Dataset;
+
+  availableAgentGroups: AgentGroup[] = [];
+
+  availableAgentPolicies: AgentPolicy[] = [];
+
+  availableSinks: Sink[] = [];
 
   constructor(
     private agentGroupsService: AgentGroupsService,
@@ -56,25 +67,57 @@ export class DatasetAddComponent {
     private route: ActivatedRoute,
     private _formBuilder: FormBuilder,
   ) {
-    this.isEdit = this.router.getCurrentNavigation().extras.state?.edit as boolean;
-    this.datasetID = this.route.snapshot.paramMap.get('id');
+    this.detailsFormGroup = this._formBuilder.group({
+      name: ['', [Validators.required, Validators.pattern('^[a-zA-Z_][a-zA-Z0-9_-]*$')]],
+    });
+    this.agentFormGroup = this._formBuilder.group({
+      agent_group_id: ['', [Validators.required]],
+    });
+    this.policyFormGroup = this._formBuilder.group({
+      agent_policy_id: ['', [Validators.required]],
+    });
+    this.sinkFormGroup = this._formBuilder.group({
+      selected_sink: ['', [Validators.required]],
+    });
 
-    this.isEdit = !!this.datasetID;
-    this.datasetLoading = this.isEdit;
+    this.dataset = this.router.getCurrentNavigation().extras.state?.dataset as Dataset || null;
+    this.datasetID = this.route.snapshot.paramMap.get('id');
+    this.isEdit = this.router.getCurrentNavigation().extras.state?.edit as boolean && !!this.datasetID;
+
+    // when editing, do not change agent group or policy
+    if (this.isEdit) {
+      this.isLoading[CONFIG.DATASET] = true;
+      this.agentFormGroup.controls.agent_group_id.disable();
+      this.policyFormGroup.controls.agent_policy_id.disable();
+    }
 
     !!this.datasetID && datasetPoliciesService.getDatasetById(this.datasetID).subscribe(resp => {
       this.dataset = resp;
-      this.datasetLoading = false;
+      this.isLoading[CONFIG.DATASET] = false;
+      this.updateForm();
     });
+  }
 
-    const { name, agent_group_id, agent_policy_id, sink_ids } = !!this.dataset ? this.dataset : {
+  getDatasetAvailableConfigList() {
+    this.getAvailableAgentGroups();
+    this.getAvailableAgentPolicies();
+    this.getAvailableSinks();
+  }
+
+  updateForm() {
+    const {
+      name, agent_group_id, agent_policy_id,
+      sink_ids,
+    } = !!this.dataset ? this.dataset : {
       name: '',
-      agent_group_id: { name: '', id: '' },
-      agent_policy_id: { name: '', id: '' },
+      agent_group_id: '',
+      agent_policy_id: '',
       sink_ids: [],
-    };
+    } as Dataset;
 
-    this.selectedSinks = sink_ids;
+    this.selectedSinks = sink_ids.map<{ id: string, name: string }>((id) => {
+      return { id, name: '' };
+    });
 
     this.detailsFormGroup = this._formBuilder.group({
       name: [name, [Validators.required, Validators.pattern('^[a-zA-Z_][a-zA-Z0-9_-]*$')]],
@@ -88,49 +131,42 @@ export class DatasetAddComponent {
     this.sinkFormGroup = this._formBuilder.group({
       selected_sink: ['', [Validators.required]],
     });
-    // when editing, do not change agent group or policy
-    if (!this.isEdit) {
-      this.getAvailableAgentGroups();
-      this.getAvailableAgentPolicies();
-      this.getAvailableSinks();
-    } else {
-      this.agentFormGroup.controls.agent_group_id.disable();
-      this.policyFormGroup.controls.agent_policy_id.disable();
-    }
-
   }
 
   getAvailableAgentGroups() {
-    this.isLoading = true;
+    this.isLoading[CONFIG.GROUPS] = true;
     const pageInfo = { ...AgentGroupsService.getDefaultPagination(), limit: 100 };
     this.agentGroupsService
       .getAgentGroups(pageInfo, false)
       .subscribe((resp: OrbPagination<AgentGroup>) => {
         this.availableAgentGroups = resp.data;
-        this.isLoading = false;
+        this.isLoading[CONFIG.GROUPS] = false;
       });
   }
 
   getAvailableAgentPolicies() {
-    this.isLoading = true;
+    this.isLoading[CONFIG.POLICIES] = true;
     const pageInfo = { ...AgentPoliciesService.getDefaultPagination(), limit: 100 };
     this.agentPoliciesService
       .getAgentsPolicies(pageInfo, false)
       .subscribe((resp: OrbPagination<AgentPolicy>) => {
         this.availableAgentPolicies = resp.data;
-        this.isLoading = false;
+        this.isLoading[CONFIG.POLICIES] = false;
       });
   }
 
   getAvailableSinks() {
-    this.isLoading = true;
+    this.isLoading[CONFIG.SINKS] = true;
     const pageInfo = { ...SinksService.getDefaultPagination(), limit: 100 };
     this.sinksService
       .getSinks(pageInfo, false)
       .subscribe((resp: OrbPagination<Sink>) => {
+        this.selectedSinks.forEach((sink) => {
+          sink.name = resp.data.find(anotherSink => anotherSink.id === sink.id).name;
+        });
         const sinkIDMap = this.selectedSinks.map(sink => sink.id);
         this.availableSinks = resp.data.filter(sink => !sinkIDMap.includes(sink.id));
-        this.isLoading = false;
+        this.isLoading[CONFIG.SINKS] = false;
       });
   }
 
