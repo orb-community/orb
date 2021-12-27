b3516e2a6d7c41cbea45a986be288a649877d626
dscabral <dcabral@daitan.com>
2021-12-27 09:35:20 -0300

    bug(sinker): wip ugly way to solve the topGeoLoc and topASN problem

--- sinker/backend/pktvisor/pktvisor.go
+++ sinker/backend/pktvisor/pktvisor.go
@@ -169,6 +169,12 @@ func makePromParticle(ctxt *context, label string, k string, v interface{}, tsLi
 }
 
 func camelToSnake(s string) string {
+	switch s {
+	case "TopGeoLoc":
+		return "top_geoLoc"
+	case "TopASN":
+		return "top_ASN"
+	}
 	var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
 	var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
 	snake := matchFirstCap.ReplaceAllString(s, "${1}_${2}")
