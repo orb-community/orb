a82fda8e0c2a10ffe9d38e95b64b5c66ef3cf5fc
mclcavalcante <mcavalcante@daitan.com>
2022-02-24 15:23:45 -0300

    wip

--- sinker/backend/pktvisor/pktvisor_test.go
+++ sinker/backend/pktvisor/pktvisor_test.go
@@ -393,6 +393,88 @@ func TestPCAPConversion(t *testing.T) {
 				},
 			},
 		},
+		"PCAPPayload_if_drops": {
+			data: []byte(`
+{
+	"policy_pcap": {
+		"pcap": {
+			"if_drops": 2
+    	}
+	}
+}`),
+			expected: prometheus.TimeSeries{
+				Labels: []prometheus.Label{
+					{
+						Name:  "__name__",
+						Value: "pcap_if_drops",
+					},
+					{
+						Name:  "instance",
+						Value: "agent-test",
+					},
+					{
+						Name:  "agent_id",
+						Value: agentID.String(),
+					},
+					{
+						Name:  "agent",
+						Value: "agent-test",
+					},
+					{
+						Name:  "policy_id",
+						Value: policyID.String(),
+					},
+					{
+						Name:  "policy",
+						Value: "policy-test",
+					},
+				},
+				Datapoint: prometheus.Datapoint{
+					Value: 2,
+				},
+			},
+		},
+		"PCAPPayload_os_drops": {
+			data: []byte(`
+{
+	"policy_pcap": {
+		"pcap": {
+			"os_drops": 2
+    	}
+	}
+}`),
+			expected: prometheus.TimeSeries{
+				Labels: []prometheus.Label{
+					{
+						Name:  "__name__",
+						Value: "pcap_os_drops",
+					},
+					{
+						Name:  "instance",
+						Value: "agent-test",
+					},
+					{
+						Name:  "agent_id",
+						Value: agentID.String(),
+					},
+					{
+						Name:  "agent",
+						Value: "agent-test",
+					},
+					{
+						Name:  "policy_id",
+						Value: policyID.String(),
+					},
+					{
+						Name:  "policy",
+						Value: "policy-test",
+					},
+				},
+				Datapoint: prometheus.Datapoint{
+					Value: 2,
+				},
+			},
+		},
 	}
 
 	for desc, c := range cases {
@@ -541,6 +623,101 @@ func TestDNSConversion(t *testing.T) {
 				},
 			},
 		},
+		"DNSPayloadCardinalityTotal": {
+			data: []byte(`
+{
+	"policy_dns": {
+		"dns": {
+        	"cardinality": {
+				"qname": 4
+        	}
+    	}
+	}
+}`),
+			expected: prometheus.TimeSeries{
+				Labels: []prometheus.Label{
+					{
+						Name:  "__name__",
+						Value: "dns_cardinality_qname",
+					},
+					{
+						Name:  "instance",
+						Value: "agent-test",
+					},
+					{
+						Name:  "agent_id",
+						Value: agentID.String(),
+					},
+					{
+						Name:  "agent",
+						Value: "agent-test",
+					},
+					{
+						Name:  "policy_id",
+						Value: policyID.String(),
+					},
+					{
+						Name:  "policy",
+						Value: "policy-test",
+					},
+				},
+				Datapoint: prometheus.Datapoint{
+					Value: 4,
+				},
+			},
+		},
+		"DNSPayloadRatesTotal": {
+			data: []byte(`
+{
+	"policy_dns": {
+		"dns": {
+        	"rates": {
+				"total": {
+					"p50": 0,
+          			"p90": 0,
+          			"p95": 2,
+          			"p99": 6
+				}
+        	}
+    	}
+	}
+}`),
+			expected: prometheus.TimeSeries{
+				Labels: []prometheus.Label{
+					{
+						Name:  "__name__",
+						Value: "dns_rates_total",
+					},
+					{
+						Name:  "instance",
+						Value: "agent-test",
+					},
+					{
+						Name:  "agent_id",
+						Value: agentID.String(),
+					},
+					{
+						Name:  "agent",
+						Value: "agent-test",
+					},
+					{
+						Name:  "policy_id",
+						Value: policyID.String(),
+					},
+					{
+						Name:  "policy",
+						Value: "policy-test",
+					},
+					{
+						Name:  "quantile",
+						Value: "0.9",
+					},
+				},
+				Datapoint: prometheus.Datapoint{
+					Value: 0,
+				},
+			},
+		},
 	}
 
 	for desc, c := range cases {
@@ -565,6 +742,196 @@ func TestDNSConversion(t *testing.T) {
 			}
 			assert.True(t, reflect.DeepEqual(c.expected.Labels, receivedLabel), fmt.Sprintf("%s: expected %v got %v", desc, c.expected.Labels, receivedLabel))
 			assert.Equal(t, c.expected.Datapoint.Value, receivedDatapoint.Value, fmt.Sprintf("%s: expected value %f got %f", desc, c.expected.Datapoint.Value, receivedDatapoint.Value))
+		})
+	}
+
+}
+
+func TestDNSRatesConversion(t *testing.T) {
+	var logger *zap.Logger
+	pktvisor.Register(logger)
+
+	ownerID, err := uuid.NewV4()
+	require.Nil(t, err, fmt.Sprintf("unexpected error: %s", err))
+
+	policyID, err := uuid.NewV4()
+	require.Nil(t, err, fmt.Sprintf("unexpected error: %s", err))
+
+	agentID, err := uuid.NewV4()
+	require.Nil(t, err, fmt.Sprintf("unexpected error: %s", err))
+
+	var agent = &pb.OwnerRes{
+		OwnerID:   ownerID.String(),
+		AgentName: "agent-test",
+	}
+
+	data := fleet.AgentMetricsRPCPayload{
+		PolicyID:   policyID.String(),
+		PolicyName: "policy-test",
+		Datasets:   nil,
+		Format:     "json",
+		BEVersion:  "1.0",
+	}
+
+	be := backend.GetBackend("pktvisor")
+
+	cases := map[string]struct {
+		data               []byte
+		expectedLabels     []prometheus.Label
+		expectedDatapoints []float64
+	}{
+		"DNSPayloadRatesTotal": {
+			data: []byte(`
+{
+	"policy_dns": {
+		"dns": {
+        	"rates": {
+				"total": {
+					"p50": 0,
+          			"p90": 1,
+          			"p95": 2,
+          			"p99": 6
+				}
+        	}
+    	}
+	}
+}`),
+			expectedLabels: []prometheus.Label{
+					{
+						Name:  "__name__",
+						Value: "dns_rates_total",
+					},
+					{
+						Name:  "instance",
+						Value: "agent-test",
+					},
+					{
+						Name:  "agent_id",
+						Value: agentID.String(),
+					},
+					{
+						Name:  "agent",
+						Value: "agent-test",
+					},
+					{
+						Name:  "policy_id",
+						Value: policyID.String(),
+					},
+					{
+						Name:  "policy",
+						Value: "policy-test",
+					},
+					{
+						Name:  "quantile",
+						Value: "0.5",
+					},
+					{
+						Name:  "__name__",
+						Value: "dns_rates_total",
+					},
+					{
+						Name:  "instance",
+						Value: "agent-test",
+					},
+					{
+						Name:  "agent_id",
+						Value: agentID.String(),
+					},
+					{
+						Name:  "agent",
+						Value: "agent-test",
+					},
+					{
+						Name:  "policy_id",
+						Value: policyID.String(),
+					},
+					{
+						Name:  "policy",
+						Value: "policy-test",
+					},
+					{
+						Name:  "quantile",
+						Value: "0.9",
+					},
+					{
+						Name:  "__name__",
+						Value: "dns_rates_total",
+					},
+					{
+						Name:  "instance",
+						Value: "agent-test",
+					},
+					{
+						Name:  "agent_id",
+						Value: agentID.String(),
+					},
+					{
+						Name:  "agent",
+						Value: "agent-test",
+					},
+					{
+						Name:  "policy_id",
+						Value: policyID.String(),
+					},
+					{
+						Name:  "policy",
+						Value: "policy-test",
+					},
+					{
+						Name:  "quantile",
+						Value: "0.95",
+					},
+					{
+						Name:  "__name__",
+						Value: "dns_rates_total",
+					},
+					{
+						Name:  "instance",
+						Value: "agent-test",
+					},
+					{
+						Name:  "agent_id",
+						Value: agentID.String(),
+					},
+					{
+						Name:  "agent",
+						Value: "agent-test",
+					},
+					{
+						Name:  "policy_id",
+						Value: policyID.String(),
+					},
+					{
+						Name:  "policy",
+						Value: "policy-test",
+					},
+					{
+						Name:  "quantile",
+						Value: "0.99",
+					},
+				},
+				expectedDatapoints: []float64{0, 1, 2, 6},
+			},
+		}
+
+	for desc, c := range cases {
+		t.Run(desc, func(t *testing.T) {
+			data.Data = c.data
+			res, err := be.ProcessMetrics(agent, agentID.String(), data)
+			require.Nil(t, err, fmt.Sprintf("%s: unexpected error: %s", desc, err))
+			var receivedLabel []prometheus.Label
+			var receivedDatapoint []float64
+
+			for _, value := range res {
+				if c.expectedLabels[0] == value.Labels[0] {
+					for _, labels := range value.Labels {
+						receivedLabel = append(receivedLabel, labels)
+					}
+					receivedDatapoint = append(receivedDatapoint, value.Datapoint.Value)
+				}
+			}
+			assert.Equal(t, c.expectedLabels, receivedLabel, fmt.Sprintf("%s: expected %v got %v", desc, c.expectedLabels, receivedLabel))
+			assert.Equal(t, c.expectedDatapoints, receivedDatapoint, fmt.Sprintf("%s: expected %v got %v", desc, c.expectedDatapoints, receivedDatapoint))
 		})
 	}
 
