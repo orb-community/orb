c37b2ef50ec18da925153c54ff168b9cdd72dadc
Guilhermo Pazuch <1490938+gpazuch@users.noreply.github.com>
2022-02-25 14:05:09 -0300

    fix(orb-ui): register redirect issue (#898)
    * wip - this return shouldn't be here and is certainly preventing the re-route and authentication
    
    * wip
    
    * on refresh page, _ps would become undefined

--- ui/src/app/pages/register/register.component.ts
+++ ui/src/app/pages/register/register.component.ts
@@ -1,5 +1,3 @@
-declare var _ps;
-
 import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
 
 import { NB_AUTH_OPTIONS, NbAuthService, NbRegisterComponent } from '@nebular/auth';
@@ -38,7 +36,6 @@ export class RegisterComponent extends NbRegisterComponent implements OnInit {
   }
 
   ngOnInit() { // In the ngOnInit() or in the constructor
-    _ps = window['_ps'];
     const el = document.getElementById('nb-global-spinner');
     if (el) {
       el.style['display'] = 'none';
@@ -58,6 +55,7 @@ export class RegisterComponent extends NbRegisterComponent implements OnInit {
 
   // Return whether to block the submission or not.
   blockSubmission() {
+    const _ps = window['_ps'];
     if (!this._psEnabled) return false;
     // Check to ensure we're able to get the Group successfully.
     if (_ps.getByKey(this._groupKey)) {
@@ -78,6 +76,8 @@ export class RegisterComponent extends NbRegisterComponent implements OnInit {
     this.errors = this.messages = [];
     this.submitted = true;
 
+    const _ps = window['_ps'];
+
     event?.preventDefault();
     if (!this.blockSubmission()) {
       // We don't need to block the form submission,
@@ -89,8 +89,8 @@ export class RegisterComponent extends NbRegisterComponent implements OnInit {
         company,
       }).subscribe(
         respReg => {
-          const first_name = this.user.fullname.split(' ')[0];
-          const last_name = this.user.fullName.replace(`${first_name} `, '');
+          const first_name = this.user.fullName.split(' ')[0];
+          const last_name = this.user.fullName.replace(`${ first_name } `, '');
           _ps.getByKey(this._groupKey).send('updated', { custom_data: { first_name, last_name } });
 
           this.submitted = false;
@@ -99,7 +99,6 @@ export class RegisterComponent extends NbRegisterComponent implements OnInit {
             this.messages = respReg.getMessages();
           } else {
             this.errors = respReg.getErrors();
-            return;
           }
 
           this.cd.detectChanges();
