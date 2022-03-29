a2451ab8f3835fc84fc018a38d21f95d99c54d9d
gpazuch <1490938+gpazuch@users.noreply.github.com>
2022-03-29 18:23:36 -0300

    wip

---
+++ ui/src/app/shared/components/orb/policy/policy-datasets/policy-datasets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-policy-datasets',
+  templateUrl: './policy-datasets.component.html',
+  styleUrls: ['./policy-datasets.component.scss'],
+})
+export class PolicyDatasetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
