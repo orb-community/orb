585a03669695db762ebf5949a60ac6cf02c40742
Guilhermo Pazuch <1490938+gpazuch@users.noreply.github.com>
2022-04-01 14:39:38 -0300

    fix(orb-ui): fix yaml policy example (#1022)

--- ui/src/app/pages/datasets/policies.agent/add/agent.policy.add.component.ts
+++ ui/src/app/pages/datasets/policies.agent/add/agent.policy.add.component.ts
@@ -103,25 +103,16 @@ export class AgentPolicyAddComponent {
     lineNumbersMinChars: 0,
   };
 
-  code = `# This is an example policy
-version: "1.0"
-
-visor:
-  policies:
-    default_view:
-      kind: collection
-      input:
-        tap: default
-        input_type: pcap
-      handlers:
-        window_config:
-          num_periods: 5
-          deep_sample_rate: 100
-        modules:
-          default_dns:
-            type: dns
-          default_net:
-            type: net`;
+  code = `handlers:
+  modules:
+    default_dns:
+      type: dns
+    default_net:
+      type: net
+input:
+  input_type: pcap
+  tap: default_pcap
+kind: collection`;
 
   // is config specified wizard mode or in YAML or JSON
   isWizard = true;
