41bed89d1a94f91429093bab91b5b8179cd3b4f2
gpazuch <1490938+gpazuch@users.noreply.github.com>
2022-05-02 17:02:26 -0300

    wip - unsure why unable to update policy interface/details.

--- ui/src/app/pages/datasets/policies.agent/view/agent.policy.view.component.ts
+++ ui/src/app/pages/datasets/policies.agent/view/agent.policy.view.component.ts
@@ -68,15 +68,34 @@ export class AgentPolicyViewComponent implements OnInit, OnDestroy {
   }
 
   save() {
+    const { format, version } = this.policy;
+
     // get values from all modified sections' forms and submit through service.
     const policyDetails = this.detailsComponent.formGroup?.value;
-    // const policyInterface = this.interfaceComponent.formControl?.value;
+    const policyInterface = this.interfaceComponent.formControl?.value;
+
+    console.table(this.editMode);
+
+    const detailsPartial = !!this.editMode.details && {
+      ...policyDetails,
+    };
+
+    const interFacePartial = !!this.editMode.interface &&
+      (format === 'yaml' ?
+      {
+        format: 'yaml',
+        // this should be refactored out.
+        policy_data: policyInterface,
+      } :
+      {
+        format: 'json',
+        policy: policyInterface,
+      });
 
     const payload = {
-      ...policyDetails,
-      format: this.policy.format,
-      policy_data: this.policy.policy_data,
-      version: 1,
+      ...detailsPartial,
+      ...interFacePartial,
+      version,
     } as AgentPolicy;
 
     this.policiesService.editAgentPolicy({ id: this.policyId, ...payload })
