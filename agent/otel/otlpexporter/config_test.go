1e62f550a5265c6df724f314c597195f03e841d9
manrodrigues <78241475+manrodrigues@users.noreply.github.com>
2022-05-24 18:00:18 -0300

    Revert otel commit (#1245)
    * Revert "wip (#1227)"
    
    This reverts commit 2c429ee42033f90b317726e5bec68cc731ab50ed.
    
    * Revert "feat(orb-ui): duplicate policy button (#1204)"
    
    This reverts commit 6354af162b336b4cfb5fc95cc3675662589583b8.
    
    * Revert "feat(agent): adapt otlphttpexporter to use mqtt (#1100)"
    
    This reverts commit 08a7da055d835d86d9736c32df8e3f300e3de350.

--- agent/otel/otlpexporter/config_test.go
+++ agent/otel/otlpexporter/config_test.go
@@ -5,20 +5,19 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component/componenttest"
-	"go.opentelemetry.io/collector/service/servicetest"
+	"go.opentelemetry.io/collector/config/configtest"
 	"path"
 	"testing"
 )
 
 func TestLoadConfig(t *testing.T) {
-	t.Skip("Configuration is not done yet")
 	factories, err := componenttest.NopFactories()
 	assert.NoError(t, err)
 
 	factory := otlpexporter.NewFactory()
 	factories.Exporters["otlp"] = factory
 
-	cfg, err := servicetest.LoadConfigAndValidate(path.Join(".", "testdata", "config.yaml"), factories)
+	cfg, err := configtest.LoadConfigAndValidate(path.Join(".", "testdata", "config.yaml"), factories)
 	require.NoError(t, err)
 	require.NotNil(t, cfg)
 }
--- agent/otel/otlpmqttexporter/doc.go
+++
@@ -1,2 +0,0 @@
-// Package otlpmqttexporter exports data by using the OTLP format to an MQTT endpoint.
-package otlpmqttexporter
