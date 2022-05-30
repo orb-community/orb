2d67857d457182655ceb3444d19e082803d06d39
manrodrigues <78241475+manrodrigues@users.noreply.github.com>
2022-05-30 18:12:59 -0300

    Tests/get output (#1273)
    * printing behavex result

---
+++ python-test/print_behavex_output.py
@@ -0,0 +1,35 @@
+import configparser
+import os
+import json
+
+
+parser = configparser.ConfigParser()
+parser.read('test_config.ini')
+configs = parser['test_config']
+
+local_orb_path = configs.get("orb_path", os.path.dirname(os.getcwd()))
+
+local_output_dir = local_orb_path + "/python-test/output/"
+
+with open(local_output_dir + "overall_status.json") as f:
+    print("-----------------------------------------------------------------------------------\n")
+    suite_result = json.load(f)['status']
+    print(f"Test suite result: {suite_result}\n")
+    print("-----------------------------------------------------------------------------------\n")
+    f.close()
+
+
+file = open(local_output_dir+"report.json")
+
+report = json.load(file)
+
+file.close()
+
+
+for feature_index,  feature in enumerate(report['features']):
+    if feature['status'] == "failed":
+        for scenario_index, scenario in enumerate(feature['scenarios']):
+            if scenario['status'] == "failed":
+                for key, value in list(scenario.items()):
+                    print(f"{key.upper()}: {value}")
+                print("-----------------------------------------------------------------------------------\n")
