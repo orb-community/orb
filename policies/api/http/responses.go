ff5500ae3019e3211ac4eda53ec436f0777c46e3
Luiz Pegoraro <luiz.pegoraro@encora.com>
2022-06-03 17:37:44 -0300

    fix(policies): add ts_created to payload and ensure to be retrieved in response.
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- policies/api/http/responses.go
+++ policies/api/http/responses.go
@@ -21,6 +21,7 @@ type policyRes struct {
 	Format        string         `json:"format,omitempty"`
 	PolicyData    string         `json:"policy_data,omitempty"`
 	Version       int32          `json:"version"`
+	Created       time.Time      `json:"ts_created"`
 	LastModified  time.Time      `json:"ts_last_modified"`
 	created       bool
 }
