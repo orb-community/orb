d1f3e85b979ae4a00f4ab9889d9dcf6577881622
Luiz Pegoraro <luiz.pegoraro@encora.com>
2022-06-21 18:06:19 -0300

    fix(agent): change the handling of message to be async, for not locking the message handling function
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- agent/rpc_from.go
+++ agent/rpc_from.go
@@ -73,26 +73,26 @@ func (a *orbAgent) handleAgentPolicies(rpc []fleet.AgentPolicyRPCPayload, fullLi
 }
 
 func (a *orbAgent) handleGroupRPCFromCore(client mqtt.Client, message mqtt.Message) {
-	go func() {
-		a.logger.Debug("Group RPC message from core", zap.String("topic", message.Topic()), zap.ByteString("payload", message.Payload()))
+	a.logger.Debug("Group RPC message from core", zap.String("topic", message.Topic()), zap.ByteString("payload", message.Payload()))
 
-		var rpc fleet.RPC
-		if err := json.Unmarshal(message.Payload(), &rpc); err != nil {
-			a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaMalformed))
-			return
-		}
-		if rpc.SchemaVersion != fleet.CurrentRPCSchemaVersion {
-			a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaVersion))
-			return
-		}
-		if rpc.Func == "" || rpc.Payload == nil {
-			a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaMalformed))
-			return
-		}
+	var rpc fleet.RPC
+	if err := json.Unmarshal(message.Payload(), &rpc); err != nil {
+		a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaMalformed))
+		return
+	}
+	if rpc.SchemaVersion != fleet.CurrentRPCSchemaVersion {
+		a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaVersion))
+		return
+	}
+	if rpc.Func == "" || rpc.Payload == nil {
+		a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaMalformed))
+		return
+	}
 
-		// dispatch
-		switch rpc.Func {
-		case fleet.AgentPolicyRPCFunc:
+	// dispatch
+	switch rpc.Func {
+	case fleet.AgentPolicyRPCFunc:
+		go func() {
 			var r fleet.AgentPolicyRPC
 			if err := json.Unmarshal(message.Payload(), &r); err != nil {
 				a.logger.Error("error decoding agent policy message from core", zap.Error(fleet.ErrSchemaMalformed))
@@ -101,26 +101,30 @@ func (a *orbAgent) handleGroupRPCFromCore(client mqtt.Client, message mqtt.Messa
 			a.handleAgentPolicies(r.Payload, r.FullList)
 			a.logger.Debug("received agent policies, marking success")
 			a.policyRequestSucceeded <- true
-		case fleet.GroupRemovedRPCFunc:
+		}()
+	case fleet.GroupRemovedRPCFunc:
+		go func() {
 			var r fleet.GroupRemovedRPC
 			if err := json.Unmarshal(message.Payload(), &r); err != nil {
 				a.logger.Error("error decoding agent group removal message from core", zap.Error(fleet.ErrSchemaMalformed))
 				return
 			}
 			a.handleAgentGroupRemoval(r.Payload)
-		case fleet.DatasetRemovedRPCFunc:
+		}()
+	case fleet.DatasetRemovedRPCFunc:
+		go func() {
 			var r fleet.DatasetRemovedRPC
 			if err := json.Unmarshal(message.Payload(), &r); err != nil {
 				a.logger.Error("error decoding dataset removal message from core", zap.Error(fleet.ErrSchemaMalformed))
 				return
 			}
 			a.handleDatasetRemoval(r.Payload)
-		default:
-			a.logger.Warn("unsupported/unhandled core RPC, ignoring",
-				zap.String("func", rpc.Func),
-				zap.Any("payload", rpc.Payload))
-		}
-	}()
+		}()
+	default:
+		a.logger.Warn("unsupported/unhandled core RPC, ignoring",
+			zap.String("func", rpc.Func),
+			zap.Any("payload", rpc.Payload))
+	}
 }
 
 func (a *orbAgent) handleAgentStop(payload fleet.AgentStopRPCPayload) {
@@ -149,26 +153,26 @@ func (a *orbAgent) handleAgentReset(payload fleet.AgentResetRPCPayload) {
 }
 
 func (a *orbAgent) handleRPCFromCore(client mqtt.Client, message mqtt.Message) {
-	go func() {
-		a.logger.Debug("RPC message from core", zap.String("topic", message.Topic()), zap.ByteString("payload", message.Payload()))
+	a.logger.Debug("RPC message from core", zap.String("topic", message.Topic()), zap.ByteString("payload", message.Payload()))
 
-		var rpc fleet.RPC
-		if err := json.Unmarshal(message.Payload(), &rpc); err != nil {
-			a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaMalformed))
-			return
-		}
-		if rpc.SchemaVersion != fleet.CurrentRPCSchemaVersion {
-			a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaVersion))
-			return
-		}
-		if rpc.Func == "" || rpc.Payload == nil {
-			a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaMalformed))
-			return
-		}
+	var rpc fleet.RPC
+	if err := json.Unmarshal(message.Payload(), &rpc); err != nil {
+		a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaMalformed))
+		return
+	}
+	if rpc.SchemaVersion != fleet.CurrentRPCSchemaVersion {
+		a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaVersion))
+		return
+	}
+	if rpc.Func == "" || rpc.Payload == nil {
+		a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaMalformed))
+		return
+	}
 
-		// dispatch
-		switch rpc.Func {
-		case fleet.GroupMembershipRPCFunc:
+	// dispatch
+	switch rpc.Func {
+	case fleet.GroupMembershipRPCFunc:
+		go func() {
 			var r fleet.GroupMembershipRPC
 			if err := json.Unmarshal(message.Payload(), &r); err != nil {
 				a.logger.Error("error decoding group membership message from core", zap.Error(fleet.ErrSchemaMalformed))
@@ -177,7 +181,9 @@ func (a *orbAgent) handleRPCFromCore(client mqtt.Client, message mqtt.Message) {
 			a.handleGroupMembership(r.Payload)
 			a.logger.Debug("received group membership, marking success")
 			a.groupRequestSucceeded <- true
-		case fleet.AgentPolicyRPCFunc:
+		}()
+	case fleet.AgentPolicyRPCFunc:
+		go func() {
 			var r fleet.AgentPolicyRPC
 			if err := json.Unmarshal(message.Payload(), &r); err != nil {
 				a.logger.Error("error decoding agent policy message from core", zap.Error(fleet.ErrSchemaMalformed))
@@ -186,24 +192,28 @@ func (a *orbAgent) handleRPCFromCore(client mqtt.Client, message mqtt.Message) {
 			a.handleAgentPolicies(r.Payload, r.FullList)
 			a.logger.Debug("received agent policies, marking success")
 			a.policyRequestSucceeded <- true
-		case fleet.AgentStopRPCFunc:
+		}()
+	case fleet.AgentStopRPCFunc:
+		go func() {
 			var r fleet.AgentStopRPC
 			if err := json.Unmarshal(message.Payload(), &r); err != nil {
 				a.logger.Error("error decoding agent stop message from core", zap.Error(fleet.ErrSchemaMalformed))
 				return
 			}
 			a.handleAgentStop(r.Payload)
-		case fleet.AgentResetRPCFunc:
+		}()
+	case fleet.AgentResetRPCFunc:
+		go func() {
 			var r fleet.AgentResetRPC
 			if err := json.Unmarshal(message.Payload(), &r); err != nil {
 				a.logger.Error("error decoding agent reset message from core", zap.Error(fleet.ErrSchemaMalformed))
 				return
 			}
 			a.handleAgentReset(r.Payload)
-		default:
-			a.logger.Warn("unsupported/unhandled core RPC, ignoring",
-				zap.String("func", rpc.Func),
-				zap.Any("payload", rpc.Payload))
-		}
-	}()
+		}()
+	default:
+		a.logger.Warn("unsupported/unhandled core RPC, ignoring",
+			zap.String("func", rpc.Func),
+			zap.Any("payload", rpc.Payload))
+	}
 }
