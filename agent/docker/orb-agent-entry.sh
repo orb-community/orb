48d817800cba9f987067bb320ee4b42e69efb6bb
etaques <97463920+etaques@users.noreply.github.com>
2022-06-29 18:50:10 -0300

    [FIX] netflow/sflow on agent entrypoint (#1383)
    * [FIX] permit multiple taps

--- agent/docker/orb-agent-entry.sh
+++ agent/docker/orb-agent-entry.sh
@@ -18,6 +18,82 @@ fi
 tmpfile=$(mktemp /tmp/orb-agent-pktvisor-conf.XXXXXX)
 trap 'rm -f "$tmpfile"' EXIT
 
+#Add defaults
+(
+cat <<END
+version: "1.0"
+
+visor:
+  taps:
+END
+) > "$tmpfile"
+
+# NetFlow tap
+if [ "${PKTVISOR_NETFLOW_BIND_ADDRESS}" = '' ]; then
+  PKTVISOR_NETFLOW_BIND_ADDRESS='0.0.0.0'
+fi
+if [ "${PKTVISOR_NETFLOW_PORT_DEFAULT}" = '' ]; then
+  PKTVISOR_NETFLOW_PORT_DEFAULT='2055'
+fi
+if [ "${PKTVISOR_NETFLOW}" = 'true' ]; then
+(
+cat <<END
+    default_netflow:
+      input_type: flow
+      config:
+        flow_type: netflow
+        port: "$PKTVISOR_NETFLOW_PORT_DEFAULT"
+        bind: "$PKTVISOR_NETFLOW_BIND_ADDRESS"
+END
+) >> "$tmpfile"
+
+  export ORB_BACKENDS_PKTVISOR_CONFIG_FILE="$tmpfile"
+fi
+
+# SFlow tap
+if [ "${PKTVISOR_SFLOW_BIND_ADDRESS}" = '' ]; then
+  PKTVISOR_SFLOW_BIND_ADDRESS='0.0.0.0'
+fi
+if [ "${PKTVISOR_SFLOW_PORT_DEFAULT}" = '' ]; then
+  PKTVISOR_SFLOW_PORT_DEFAULT='6343'
+fi
+if [ "${PKTVISOR_SFLOW}" = 'true' ]; then
+(
+cat <<END
+    default_sflow:
+      input_type: flow
+      config:
+        flow_type: sflow
+        port: "$PKTVISOR_SFLOW_PORT_DEFAULT"
+        bind: "$PKTVISOR_SFLOW_BIND_ADDRESS"
+END
+) >> "$tmpfile"
+
+  export ORB_BACKENDS_PKTVISOR_CONFIG_FILE="$tmpfile"
+fi
+
+# DNS TAP
+if [ "${PKTVISOR_DNSTAP_BIND_ADDRESS}" = '' ]; then
+  PKTVISOR_DNSTAP_BIND_ADDRESS='0.0.0.0'
+fi
+if [ "${PKTVISOR_DNSTAP_PORT_DEFAULT}" = '' ]; then
+  PKTVISOR_DNSTAP_PORT_DEFAULT='6000'
+fi
+
+if [ "${PKTVISOR_DNSTAP}" = 'true' ]; then
+(
+cat <<END
+    default_dnstap:
+      input_type: dnstap
+      config:
+        tcp: "${PKTVISOR_DNSTAP_BIND_ADDRESS}:${PKTVISOR_DNSTAP_PORT_DEFAULT}"
+
+END
+) >> "$tmpfile"
+
+  export ORB_BACKENDS_PKTVISOR_CONFIG_FILE="$tmpfile"
+fi
+
 # simplest: specify just interface, creates tap named "default_pcap"
 # PKTVISOR_PCAP_IFACE_DEFAULT=en0
 # special case: if the iface is "mock", then use "mock" pcap source
@@ -27,17 +103,13 @@ fi
 if [[ -n "${PKTVISOR_PCAP_IFACE_DEFAULT}" ]]; then
 (
 cat <<END
-version: "1.0"
-
-visor:
-  taps:
     default_pcap:
       input_type: pcap
       config:
         iface: "$PKTVISOR_PCAP_IFACE_DEFAULT"
         $MAYBE_MOCK
 END
-) >"$tmpfile"
+) >>"$tmpfile"
 
   export ORB_BACKENDS_PKTVISOR_CONFIG_FILE="$tmpfile"
 fi
@@ -45,7 +117,6 @@ fi
 # or specify pair of TAPNAME:IFACE
 # TODO allow multiple, split on comma
 # PKTVISOR_PCAP_IFACE_TAPS=default_pcap:en0
-
 if [ $# -eq 0 ]; then
   exec "$orb_agent_bin" run
 else
