d43ad382aeecda2dfc03d1f4998d18b98a97cafd
mclcavalcante <mariana.cavalcante@encora.com>
2022-07-06 12:58:08 -0300

    docs(fleet): fix function description

--- fleet/agents.go
+++ fleet/agents.go
@@ -139,7 +139,7 @@ type AgentRepository interface {
 	SetStaleStatus(ctx context.Context, minutes time.Duration) (int64, error)
 	// RetrieveAgentInfoByChannelID gRPC version to retrieve ownerID, name and agent tags by a provided channelID
 	RetrieveAgentInfoByChannelID(ctx context.Context, channelID string) (Agent, error)
-	// RetrieveCountGroupAgents retrieve the total matching agents by tags
+	// RetrieveCountGroupAgents retrieve the total agents of an agent group
 	RetrieveCountGroupAgents(ctx context.Context, owner string, tags types.Tags) (int, error)
 }
 
