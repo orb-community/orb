eac1c86e211a39bb2c43df82b2dbd9832a4e1ce6
root <etaques@daitan.com>
2022-07-15 19:10:26 -0300

    feat(ops) test

--- .github/actions/slack-post/entrypoint.sh
+++ .github/actions/slack-post/entrypoint.sh
@@ -2,12 +2,13 @@
 
 function run() {
   echo "========================= generating changelog ========================="
+  AUX="${INPUT_GITHUB_REPO}"
+  DIR="${AUX##/*/}"
   cd /tmp/
   git clone -b $INPUT_BRANCH "https://github.com/${INPUT_GITHUB_REPO}"
-  cd $INPUT_DIR
+  cd $DIR
   echo "${INPUT_HEADER}" > CHANGELOG
   echo "`git log --pretty=format:"$ad• %s [%an]" --since=7.days`" >> CHANGELOG
-  cat CHANGELOG
   result=$(cat CHANGELOG)
   export CHANGELOG_RESULT=$result
 }
