205fe782fc4d36c8b75ba370fd9a742085b06110
Luiz Henrique Pegoraro <luiz.pegoraro@encora.com>
2022-07-18 18:29:08 -0300

    feat(agent): remove restart comms indefinetely (#1475)
    * feat(agent): remove restart comms indefinetely
    
    * feat(agent): fix message of log
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): add log of topics
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): add log of topics
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): move log to debug level
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): enable resumeSub
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): attempt of backoff
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): simpler approach
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): simpler approach
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): simpler approach
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): simpler approach
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): simpler approach
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): simpler approach
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): graceful shutdown
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): rewording
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): add log fatal
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): remove timeout
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): add timeout to 5 minutes
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): remove backoff
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): remove backoff counter
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- agent/rpc_from.go
+++ agent/rpc_from.go
@@ -75,7 +75,6 @@ func (a *orbAgent) handleAgentPolicies(rpc []fleet.AgentPolicyRPCPayload, fullLi
 func (a *orbAgent) handleGroupRPCFromCore(client mqtt.Client, message mqtt.Message) {
 	go func() {
 		a.logger.Debug("Group RPC message from core", zap.String("topic", message.Topic()), zap.ByteString("payload", message.Payload()))
-
 		var rpc fleet.RPC
 		if err := json.Unmarshal(message.Payload(), &rpc); err != nil {
 			a.logger.Error("error decoding RPC message from core", zap.Error(fleet.ErrSchemaMalformed))
