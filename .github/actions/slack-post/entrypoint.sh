9d02198bdfd740ac3b24a0c626cc597ee5f86c1e
etaques <97463920+etaques@users.noreply.github.com>
2022-07-18 10:23:13 -0300

    feat(ops) add slack post notification message (#1495)

---
+++ .github/actions/slack-post/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function run() {
+  echo "========================= generating changelog ========================="
+  AUX="/${INPUT_GITHUB_REPO}"
+  DIR=$(echo "${AUX##/*/}")
+  cd /tmp/
+  git clone -b $INPUT_BRANCH "https://github.com/${INPUT_GITHUB_REPO}"
+  cd $DIR
+  echo "${INPUT_HEADER}" > CHANGELOG
+  echo "`git log --pretty=format:"$ad• %s [%an]" --since=7.days`" >> CHANGELOG
+  result=$(cat CHANGELOG)
+  export CHANGELOG_RESULT=$result
+}
+
+function comment() {
+  echo "========================= Posting on slack ========================="
+  curl -d "text=${CHANGELOG_RESULT}" -d "channel=${INPUT_SLACK_CHANNEL}" -H "Authorization: Bearer ${INPUT_SLACK_API_TOKEN}" -X POST https://slack.com/api/chat.postMessage
+}
+
+run
+comment
