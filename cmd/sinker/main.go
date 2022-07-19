a12c9775112a75f47bf56a0d77c44d5f8914af50
Luiz Henrique Pegoraro <luiz.pegoraro@encora.com>
2022-07-19 10:25:50 -0300

    feat(sinker): add debug and omitempty unset metrics from pktvisor (#1415)
    * feat(sinker): add log as json and add debug for sinker
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(sinker): closely check the error and set to always generate log as json for better reading format.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(sinker): add omitempty to StatSnapshot structure.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(sinker): add debug logic to pass the information.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(sinker): add debug to ts building
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(sinker): remove debug logs.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- cmd/sinker/main.go
+++ cmd/sinker/main.go
@@ -29,6 +29,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	jconfig "github.com/uber/jaeger-client-go/config"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"io"
@@ -38,6 +39,7 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -64,11 +66,23 @@ func main() {
 
 	// main logger
 	var logger *zap.Logger
-	if svcCfg.LogLevel == "debug" {
-		logger, _ = zap.NewDevelopment()
-	} else {
-		logger, _ = zap.NewProduction()
+	atomicLevel := zap.NewAtomicLevel()
+	switch strings.ToLower(svcCfg.LogLevel) {
+	case "debug":
+		atomicLevel.SetLevel(zap.DebugLevel)
+	case "warn":
+		atomicLevel.SetLevel(zap.WarnLevel)
+	case "info":
+		atomicLevel.SetLevel(zap.InfoLevel)
+	default:
+		atomicLevel.SetLevel(zap.InfoLevel)
 	}
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		os.Stdout,
+		atomicLevel,
+	)
+	logger = zap.New(core, zap.AddCaller())
 	defer logger.Sync() // flushes buffer, if any
 
 	// only needed for mainflux interfaces
