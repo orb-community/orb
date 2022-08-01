c52e2d277a338ec7ed40d71418f3efb82c4295c3
Luiz Pegoraro <luiz.pegoraro@encora.com>
2022-08-01 11:56:06 -0300

    feat(otelcol): wip attributeprocessor
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- otelcollector/components/processors.go
+++ otelcollector/components/processors.go
@@ -7,21 +7,34 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/attributesprocessor"
 	"go.opentelemetry.io/collector/component"
 	otelconfig "go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/otel/metric/global"
+	"go.opentelemetry.io/otel/trace"
 )
 
-func GetAttributeProcessorWithOwnerAndSinkData(ctx context.Context, factories component.Factories) error {
+func GetAttributeProcessorWithOwnerAndSinkData(ctx context.Context, factories component.Factories, nextConsumer consumer.Metrics) error {
 	logger := config.LoggerFromContext(ctx)
 	slog := logger.Sugar()
 	name := "attributeprocessor"
-	_ = context.WithValue(ctx, "name", name)
-	slog.Debug("create processor", name, "")
+	subCtx := context.WithValue(ctx, "name", name)
+	slog.Debug("create processor:", name)
 	factory := factories.Processors[otelconfig.Type(name)]
 	cfg := factory.CreateDefaultConfig().(*attributesprocessor.Config)
+
 	cfg.Actions = []attraction.ActionKeyValue{
 		{Key: "ownerID", Value: "???", FromAttribute: "???", Action: "insert"}, // Mainflux Owner ID
 		{Key: "agentID", Value: "???", FromAttribute: "???", Action: "insert"}, // Agent ID
 		{Key: "sinks", Value: "???", FromAttribute: "???", Action: "insert"},   // Sink slice with id and config metadata
 	}
+	set := component.ProcessorCreateSettings{
+		TelemetrySettings: component.TelemetrySettings{
+			Logger:         logger,
+			TracerProvider: trace.NewNoopTracerProvider(),
+			MeterProvider:  global.MeterProvider(),
+		},
+		BuildInfo: component.BuildInfo{},
+	}
+	factory.CreateMetricsProcessor(subCtx, set, cfg, nextConsumer)
 
 	return nil
 }
