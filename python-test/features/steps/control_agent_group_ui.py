dd4af7f6e859df607a270853bc78f58245ddca1c
pbrasileiro <paulab@ec.ai>
2022-08-01 14:54:06 -0300

    add a group with description

--- python-test/features/steps/control_agent_group_ui.py
+++ python-test/features/steps/control_agent_group_ui.py
@@ -14,6 +14,7 @@ from page_objects import *
 configs = TestConfig.configs()
 orb_url = configs.get('orb_url')
 agent_group_name_prefix = "test_agent_group_name_"
+agent_group_description_prefix = "test_agent_group_description_"
 
 
 @given('the user clicks on new agent group on left menu')
@@ -53,7 +54,7 @@ def create_agent_through_the_agent_group_page(context, orb_tags):
         EC.text_to_be_present_in_element((By.CSS_SELECTOR, "span.title"), 'Agent Group successfully created'))
 
 
-@then("the new agent group is shown on the datatable")
+@then('the new agent group is shown on the datatable')
 def check_status_on_orb_ui(context):
     context.driver.get(f"{orb_url}/pages/fleet/groups")
     group = find_element_on_agent_group_datatable(context.driver, DataTable.agent_group(context.agent_group_name))
@@ -107,3 +108,32 @@ def find_element_on_agent_group_datatable(driver, xpath):
         element = WebDriverWait(driver, 2).until(
             EC.presence_of_all_elements_located((By.XPATH, xpath)))
     return element[0]
+
+
+@when('a new agent group with description is created through the UI with {orb_tags} orb tag')
+def create_agent_through_the_agent_group_page(context, orb_tags):
+    context.orb_tags = create_tags_set(orb_tags)
+    WebDriverWait(context.driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH, AgentGroupPage.new_agent_group_button())), message="Unable to click on new agent group"
+                                                                                       " button").click()
+    WebDriverWait(context.driver, 5).until(EC.url_to_be(f"{orb_url}/pages/fleet/groups/add"), message="Orb add"
+                                                                                                           "agent group "
+                                                                                                           "page not "
+                                                                                                           "available")
+    context.agent_group_name = agent_group_name_prefix + random_string(10)
+    input_text_by_xpath(AgentGroupPage.agent_group_name(), context.agent_group_name, context)
+    context.agent_group_description = agent_group_description_prefix + random_string(10)
+    input_text_by_xpath(AgentGroupPage.agent_group_description(), context.agent_group_description, context)
+    WebDriverWait(context.driver, 3).until(
+        EC.element_to_be_clickable((By.XPATH, UtilButton.next_button()))).click()
+    for tag_key, tag_value in context.orb_tags.items():
+        input_text_by_xpath(AgentGroupPage.agent_group_tag_key(), tag_key, context)
+        input_text_by_xpath(AgentGroupPage.agent_group_tag_value(), tag_value, context)
+        WebDriverWait(context.driver, 3).until(
+            EC.element_to_be_clickable((By.XPATH, AgentGroupPage.agent_group_add_tag_button()))).click()
+    WebDriverWait(context.driver, 3).until(
+        EC.element_to_be_clickable((By.XPATH, UtilButton.next_button()))).click()
+    WebDriverWait(context.driver, 3).until(
+        EC.element_to_be_clickable((By.XPATH, UtilButton.save_button()))).click()
+    WebDriverWait(context.driver, 3).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, "span.title"), 'Agent Group successfully created'))
