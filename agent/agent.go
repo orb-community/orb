3983d8dede6aa5f81c5f23d74c640bf804ab1605
Luiz Pegoraro <luiz.pegoraro@encora.com>
2022-08-23 17:55:51 -0300

    fix(agent): force reconnection in reset
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- agent/agent.go
+++ agent/agent.go
@@ -230,6 +230,8 @@ func (a *orbAgent) RestartBackend(ctx context.Context, name string, reason strin
 func (a *orbAgent) restartComms(ctx context.Context) error {
 	if a.client != nil && a.client.IsConnected() {
 		a.unsubscribeGroupChannels()
+		a.client.Disconnect(250)
+		a.client = nil
 	}
 	ccm, err := cloud_config.New(a.logger, a.config, a.db)
 	if err != nil {
