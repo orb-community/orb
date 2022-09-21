8fcaefe75da4b0d7ddf5fa951a67e631826fe2da
Luiz Pegoraro <luiz.pegoraro@encora.com>
2022-09-21 16:52:08 -0300

    chore(build): skip decode test, since it is local only.
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- sinker/otel/orbreceiver/encoder_test.go
+++ sinker/otel/orbreceiver/encoder_test.go
@@ -7,6 +7,9 @@ import (
 )
 
 func Test_decodeTestDataFiles(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping because this test is supposed to run locally not in build")
+	}
 	dir := "/home/lpegoraro/workspace/orb/sinker/otel/orbreceiver/testdata"
 	files, err := os.ReadDir(dir)
 	if err != nil {
