61d330447ef870e3e07447694c383f5e6af617e5
Luiz Pegoraro <luiz.pegoraro@encora.com>
2022-10-10 16:39:00 -0300

    feat(sinks/sinker): finish testing with correct validated config.
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- sinker/config_builder.go
+++ sinker/config_builder.go
@@ -4,15 +4,17 @@ import (
 	"context"
 	"fmt"
 	"gopkg.in/yaml.v2"
+	"strings"
 )
 
 // ReturnConfigYamlFromSink this is the main method, which will generate the YAML file from the
 func ReturnConfigYamlFromSink(_ context.Context, kafkaUrlConfig, sinkId, sinkUrl, sinkUsername, sinkPassword string) (string, error) {
+
 	config := OtelConfigFile{
 		Receivers: Receivers{
 			Kafka: KafkaReceiver{
 				Brokers:         []string{kafkaUrlConfig},
-				Topic:           fmt.Sprintf("otlp_metrics/%s", sinkId),
+				Topic:           fmt.Sprintf("otlp_metrics-%s", sinkId),
 				ProtocolVersion: "2.0.0", // Leaving default of over 2.0.0
 			},
 		},
@@ -32,25 +34,25 @@ func ReturnConfigYamlFromSink(_ context.Context, kafkaUrlConfig, sinkId, sinkUrl
 				Endpoint: sinkUrl,
 				auth: struct {
 					Authenticator string `json:"authenticator" yaml:"authenticator"`
-				}{Authenticator: "basic_auth/exporter"},
+				}{Authenticator: "basicauth/exporter"},
 			},
 		},
 		Service: ServiceConfig{
-			Extensions: "[pprof, basic_auth/exporter]",
+			Extensions: []string{"pprof", "basicauth/exporter"},
 			Pipelines: struct {
 				Metrics struct {
-					Receivers  string  `json:"receivers" yaml:"receivers"`
-					Processors *string `json:"processors,omitempty" yaml:"processors"`
-					Exporters  string  `json:"exporters" yaml:"exporters"`
+					Receivers  []string `json:"receivers" yaml:"receivers"`
+					Processors []string `json:"processors,omitempty" yaml:"processors,omitempty"`
+					Exporters  []string `json:"exporters" yaml:"exporters"`
 				} `json:"metrics" yaml:"metrics"`
 			}{
 				Metrics: struct {
-					Receivers  string  `json:"receivers" yaml:"receivers"`
-					Processors *string `json:"processors,omitempty" yaml:"processors"`
-					Exporters  string  `json:"exporters" yaml:"exporters"`
+					Receivers  []string `json:"receivers" yaml:"receivers"`
+					Processors []string `json:"processors,omitempty" yaml:"processors,omitempty"`
+					Exporters  []string `json:"exporters" yaml:"exporters"`
 				}{
-					Receivers: "[kafka]",
-					Exporters: "[prometheusremotewrite]",
+					Receivers: []string{"kafka"},
+					Exporters: []string{"prometheusremotewrite"},
 				},
 			},
 		},
@@ -60,14 +62,15 @@ func ReturnConfigYamlFromSink(_ context.Context, kafkaUrlConfig, sinkId, sinkUrl
 		return "", err
 	}
 	returnedString := "---\n" + string(marshal)
+	returnedString = strings.TrimRight(returnedString, `'`)
 	return returnedString, nil
 
 }
 
 type OtelConfigFile struct {
 	Receivers  Receivers     `json:"receivers" yaml:"receivers"`
-	Processors *Processors   `json:"processors,omitempty" yaml:"processors"`
-	Extensions *Extensions   `json:"extensions,omitempty" yaml:"extensions"`
+	Processors *Processors   `json:"processors,omitempty" yaml:"processors,omitempty"`
+	Extensions *Extensions   `json:"extensions,omitempty" yaml:"extensions,omitempty"`
 	Exporters  Exporters     `json:"exporters" yaml:"exporters"`
 	Service    ServiceConfig `json:"service" yaml:"service"`
 }
@@ -87,11 +90,11 @@ type Processors struct {
 }
 
 type Extensions struct {
-	HealthCheckExtConfig *HealthCheckExtension `json:"health_check,omitempty" yaml:"health_check"`
-	PProf                *PProfExtension       `json:"pprof,omitempty" yaml:"pprof"`
-	ZPages               *ZPagesExtension      `json:"zpages,omitempty" yaml:"zpages"`
+	HealthCheckExtConfig *HealthCheckExtension `json:"health_check,omitempty" yaml:"health_check,omitempty"`
+	PProf                *PProfExtension       `json:"pprof,omitempty" yaml:"pprof,omitempty"`
+	ZPages               *ZPagesExtension      `json:"zpages,omitempty" yaml:"zpages,omitempty"`
 	// Exporters Authentication
-	BasicAuth *BasicAuthenticationExtension `json:"basic_auth/exporter,omitempty" yaml:"basic_auth/exporter"`
+	BasicAuth *BasicAuthenticationExtension `json:"basicauth/exporter,omitempty" yaml:"basicauth/exporter,omitempty"`
 }
 
 type HealthCheckExtension struct {
@@ -99,7 +102,7 @@ type HealthCheckExtension struct {
 		Enabled          bool   `json:"enabled" yaml:"enabled"`
 		Interval         string `json:"interval" yaml:"interval"`
 		FailureThreshold int32  `json:"exporter_failure_threshold" yaml:"exporter_failure_threshold"`
-	} `json:"check_collector_pipeline" yaml:"check_collector_pipeline"`
+	} `json:"check_collector_pipeline,omitempty" yaml:"check_collector_pipeline,omitempty"`
 }
 
 type PProfExtension struct {
@@ -118,7 +121,7 @@ type BasicAuthenticationExtension struct {
 }
 
 type Exporters struct {
-	PrometheusRemoteWrite *PrometheusRemoteWriteExporterConfig `json:"prometheusremotewrite,omitempty" yaml:"prometheusremotewrite"`
+	PrometheusRemoteWrite *PrometheusRemoteWriteExporterConfig `json:"prometheusremotewrite,omitempty" yaml:"prometheusremotewrite,omitempty"`
 }
 
 type PrometheusRemoteWriteExporterConfig struct {
@@ -129,12 +132,12 @@ type PrometheusRemoteWriteExporterConfig struct {
 }
 
 type ServiceConfig struct {
-	Extensions string `json:"extensions,omitempty" yaml:"extensions"`
+	Extensions []string `json:"extensions,omitempty" yaml:"extensions,omitempty"`
 	Pipelines  struct {
 		Metrics struct {
-			Receivers  string  `json:"receivers" yaml:"receivers"`
-			Processors *string `json:"processors,omitempty" yaml:"processors"`
-			Exporters  string  `json:"exporters" yaml:"exporters"`
+			Receivers  []string `json:"receivers" yaml:"receivers"`
+			Processors []string `json:"processors,omitempty" yaml:"processors,omitempty"`
+			Exporters  []string `json:"exporters" yaml:"exporters"`
 		} `json:"metrics" yaml:"metrics"`
 	} `json:"pipelines" yaml:"pipelines"`
 }
