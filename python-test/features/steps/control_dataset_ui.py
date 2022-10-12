525c142536d9093816711d0565c2dcb9cea5d805
Amanda Rodrigues da Silva <arodrigues@daitan.com>
2022-10-12 14:33:01 -0300

    click_button_resilient

--- python-test/features/steps/control_dataset_ui.py
+++ python-test/features/steps/control_dataset_ui.py
@@ -20,29 +20,35 @@ def create_dataset_through_ui(context):
     for sink_id in context.existent_sinks_id:
         sink = get_sink(context.token, sink_id)
         sinks_names.append(sink['name'])
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, PolicyPage.new_dataset_button()))).click()
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, DatasetModal.agent_group()))).click()
+    button_was_clicked = button_click_by_xpath(PolicyPage.new_dataset_button(), context.driver,
+                                               "Unable to click on new dataset button")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on new dataset button")
+    button_was_clicked = button_click_by_xpath(DatasetModal.agent_group(), context.driver,
+                                               "Unable to click on button of agent groups on dataset modal")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on button of agent groups on dataset modal")
     groups_options = get_selector_options(context.driver)
     ActionChains(context.driver).move_to_element(groups_options[group_name]).perform()
     WebDriverWait(context.driver, time_webdriver_wait).until(
         EC.element_to_be_clickable(groups_options[group_name])).click()
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, DatasetModal.sinks_selector_button()))).click()
+    button_was_clicked = button_click_by_xpath(DatasetModal.sinks_selector_button(), context.driver,
+                                               "Unable to click on on sink selector button dataset modal")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on sink selector button on dataset modal")
     sinks_options = get_selector_options(context.driver)
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, DatasetModal.sinks_selector_button()))).click()
+    button_was_clicked = button_click_by_xpath(DatasetModal.sinks_selector_button(), context.driver,
+                                               "Unable to click on on sink selector button dataset modal (2)")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on sink selector button on dataset modal (2)")
     for sink in sinks_names:
-        WebDriverWait(context.driver, time_webdriver_wait).until(
-            EC.element_to_be_clickable((By.XPATH, DatasetModal.sinks_selector_button()))).click()
+        button_was_clicked = button_click_by_xpath(DatasetModal.sinks_selector_button(), context.driver,
+                                                   "Unable to click on on sink selector button dataset modal (3)")
+        assert_that(button_was_clicked, equal_to(True), "Unable to click on sink selector button on dataset modal (3)")
         ActionChains(context.driver).move_to_element(sinks_options[sink]).perform()
         WebDriverWait(context.driver, time_webdriver_wait).until(
             EC.element_to_be_clickable(sinks_options[sink])).click()
     dataset_name = dataset_name_prefix + random_string()
     input_text_by_xpath(DatasetModal.dataset_name(), dataset_name, context.driver)
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, UtilButton.save_button()))).click()
+    button_was_clicked = button_click_by_xpath(UtilButton.save_button(), context.driver,
+                                               "Unable to click on save dataset modal")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on save dataset modal")
     WebDriverWait(context.driver, 3).until(
         EC.text_to_be_present_in_element((By.CSS_SELECTOR, "span.title"), 'Dataset successfully created'),
         message="Confirmation span of dataset creation not displayed")
@@ -60,13 +66,16 @@ def remove_dataset(context):
     policy_id = context.policy['id']
     policy_view_page = f"{orb_url}/pages/datasets/policies/view/{policy_id}"
     context.driver, current_url = go_to_page(policy_view_page, driver=context.driver)
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, PolicyPage.dataset_details_modal(context.dataset['name'])))).click()
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, PolicyPage.remove_dataset_button()))).click()
+    button_was_clicked = button_click_by_xpath(PolicyPage.dataset_details_modal(context.dataset['name']),
+                                               context.driver, "Unable to click on dataset details modal")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on dataset details modal")
+    button_was_clicked = button_click_by_xpath(PolicyPage.remove_dataset_button(),
+                                               context.driver, "Unable to click on remove dataset button")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on remove dataset button")
     input_text_by_xpath(Dataset.dataset_name(), context.dataset['name'], context.driver)
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, Dataset.dataset_remove_confirmation()))).click()
+    button_was_clicked = button_click_by_xpath(Dataset.dataset_remove_confirmation(),
+                                               context.driver, "Unable to click on remove dataset confirmation")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on remove dataset confirmation")
     WebDriverWait(context.driver, 3).until(
         EC.text_to_be_present_in_element((By.CSS_SELECTOR, "span.title"), 'Dataset successfully deleted'),
         message="Confirmation span of dataset remove not displayed")
@@ -81,13 +90,16 @@ def edit_dataset_UI_using_multiple_sinks(context):
     for sink_id in context.existent_sinks_id:
         sink = get_sink(context.token, sink_id)
         sinks_names.append(sink['name'])
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, PolicyPage.dataset_details_modal(context.dataset['name'])))).click()
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, DatasetModal.sinks_selector_button()))).click()
+    button_was_clicked = button_click_by_xpath(PolicyPage.dataset_details_modal(context.dataset['name']),
+                                               context.driver, "Unable to open dataset modal")
+    assert_that(button_was_clicked, equal_to(True), "Unable to open dataset modal")
+    button_was_clicked = button_click_by_xpath(DatasetModal.sinks_selector_button(),
+                                               context.driver, "Unable to click on sink selector button")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on sink selector button")
     sinks_options = get_selector_options(context.driver)
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, DatasetModal.sinks_selector_button()))).click()
+    button_was_clicked = button_click_by_xpath(DatasetModal.sinks_selector_button(),
+                                               context.driver, "Unable to click on sink selector button (2)")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on sink selector button (2)")
     selected_sinks = \
         WebDriverWait(context.driver,
                       time_webdriver_wait).until(EC.presence_of_all_elements_located((By.XPATH,
@@ -97,8 +109,9 @@ def edit_dataset_UI_using_multiple_sinks(context):
         selected_sinks_names.append(sink.text)
     sinks_names = list(set(sinks_names) - set(selected_sinks_names))
     for sink in sinks_names:
-        WebDriverWait(context.driver, time_webdriver_wait).until(
-            EC.element_to_be_clickable((By.XPATH, DatasetModal.sinks_selector_button()))).click()
+        button_was_clicked = button_click_by_xpath(DatasetModal.sinks_selector_button(),
+                                                   context.driver, "Unable to click on sink selector button (3)")
+        assert_that(button_was_clicked, equal_to(True), "Unable to click on sink selector button (3)")
         ActionChains(context.driver).move_to_element(sinks_options[sink]).perform()
         WebDriverWait(context.driver, time_webdriver_wait).until(
             EC.element_to_be_clickable(sinks_options[sink])).click()
@@ -106,8 +119,9 @@ def edit_dataset_UI_using_multiple_sinks(context):
         EC.presence_of_element_located((By.XPATH, DatasetModal.dataset_name()))).clear()
     dataset_name = dataset_name_prefix + random_string(10)
     input_text_by_xpath(DatasetModal.dataset_name(), dataset_name, context.driver)
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, UtilButton.save_button()))).click()
+    button_was_clicked = button_click_by_xpath(UtilButton.save_button(),
+                                               context.driver, "Unable to click on save dataset button")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on save dataset button")
     WebDriverWait(context.driver, 3).until(
         EC.text_to_be_present_in_element((By.CSS_SELECTOR, "span.title"), 'Dataset successfully updated'),
         message="Confirmation span of dataset creation not displayed")
@@ -122,8 +136,9 @@ def edit_dataset_UI_using_multiple_sinks(context):
 
 @step("{amount_of_sinks} sinks are linked to the dataset and the new name is displayed")
 def check_amount_sinks_ui(context, amount_of_sinks):
-    WebDriverWait(context.driver, time_webdriver_wait).until(
-        EC.element_to_be_clickable((By.XPATH, PolicyPage.dataset_details_modal(context.dataset['name'])))).click()
+    button_was_clicked = button_click_by_xpath(PolicyPage.dataset_details_modal(context.dataset['name']),
+                                               context.driver, "Unable to click on dataset modal")
+    assert_that(button_was_clicked, equal_to(True), "Unable to click on dataset modal")
     selected_sinks = \
         WebDriverWait(context.driver,
                       time_webdriver_wait).until(EC.presence_of_all_elements_located((By.XPATH,
