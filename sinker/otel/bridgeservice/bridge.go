ed0c8efdd8c13c6b11c77ccd2bca253da87c57a8
etaques <97463920+etaques@users.noreply.github.com>
2022-11-03 11:46:36 -0300

    [FIX] logs information on receiver (#1968)
    * [FIX] logs
    
    * [FIX] logs
    
    * add strings

--- sinker/otel/bridgeservice/bridge.go
+++ sinker/otel/bridgeservice/bridge.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ns1labs/orb/sinker/config"
 	"go.uber.org/zap"
 	"time"
+	"strings"
 )
 
 type BridgeService interface {
@@ -73,7 +74,7 @@ func (bs *SinkerOtelBridgeService) GetSinkIdsFromAgentGroups(ctx context.Context
 		OwnerID:  mfOwnerId,
 	})
 	if err != nil {
-		bs.logger.Error("unable to retrieve policies from agent groups", zap.Error(err))
+		bs.logger.Info("unable to retrieve policies from agent groups ID=" + strings.Join(agentGroupIds, ", "))
 		return nil, err
 	}
 	mapSinkIdPolicy := make(map[string]string)
