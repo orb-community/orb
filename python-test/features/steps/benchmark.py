cadecc859cdfbe1c0fc0bae493aaddecb9d72429
Luiz Pegoraro <luiz.pegoraro@encora.com>
2022-11-25 18:32:17 -0300

    feat(test): implement a different report more stable.
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- python-test/features/steps/benchmark.py
+++ python-test/features/steps/benchmark.py
@@ -11,10 +11,17 @@ def monitor_docker_stats_during(context, monitor_time):
     started = datetime.now().timestamp()
     monitored_duration = 0
     event = threading.Event()
-    while not event.is_set() and monitored_duration < int(monitor_time):
+    while not event.is_set() and monitored_duration < int(monitor_time *  3600):
         container = client.containers.get(context.container_id)
         print("Monitor docker stats")
         statistics = container.stats(stream=False)
-        print(statistics)
+        used_memory = int(statistics.memory_stats.usage) - int(statistics.memory_stats.stats.cache)
+        available_memory = int(statistics.memory_stats.limit)
+        cpu_delta = int(statistics.cpu_stats.cpu_usage.total_usage) - int(statistics.precpu_stats.cpu_usage.total_usage)
+        print("Memory Usage %" + str((used_memory / available_memory) * 100.0))
+        print("CPU Delta " + str(cpu_delta))
+        print("Memory Stats" + statistics["memory_stats"])
+        print("CPU Stats" + statistics["cpu_stats"])
+        print("PreCPU Stats" + statistics["precpu_stats"])
         event.wait(30)
         monitored_duration = datetime.now().timestamp() - started
