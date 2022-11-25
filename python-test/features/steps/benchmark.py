23ee6fda3942cc4c17a1f9864e3cbeaa1df3b630
Luiz Pegoraro <luiz.pegoraro@encora.com>
2022-11-25 18:10:50 -0300

    feat(test): add benchmark testing, attempt of extracting docker stats, will change for stream.
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- python-test/features/steps/benchmark.py
+++ python-test/features/steps/benchmark.py
@@ -1,23 +1,20 @@
-import time
-
+from datetime import datetime
+import threading
 import docker
 from behave import then, step
 client = docker.from_env()
-from utils import threading_wait_until
+
 
 
 @step("monitor the activity of memory usage during {monitor_time} minutes")
 def monitor_docker_stats_during(context, monitor_time):
+    started = datetime.now().timestamp()
     monitored_duration = 0
-    while monitored_duration >= int(monitor_time):
+    event = threading.Event()
+    while not event.is_set() and monitored_duration < int(monitor_time):
         container = client.containers.get(context.container_id)
-        print_stats_and_wait(container, wait_time=30)
-        monitored_duration += 0.5
-
-
-@threading_wait_until
-def print_stats_and_wait(container):
-    print("Monitor docker stats")
-    statistics = container.stats(stream=False)
-    print(statistics)
-    time.sleep(30)
+        print("Monitor docker stats")
+        statistics = container.stats(stream=False)
+        print(statistics)
+        event.wait(30)
+        monitored_duration = datetime.now().timestamp() - started
