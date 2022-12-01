9b574be290d3e646fa20d7278d77d406737e9897
Luiz Henrique Pegoraro <luiz.pegoraro@encora.com>
2022-12-01 13:06:49 -0300

    fix(agent): fix high CPU usage on agent, and add benchmark and profiling tests to validate. (#2017)
    * feat(test): add benchmark.feature
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(Makefile): use -short in makefile
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * fix(test): wip benchmark.py
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): add benchmark testing, attempt of extracting docker stats, will change for stream.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): add benchmark testing, attempt of extracting docker stats, will change for stream.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): implement a different report more stable.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): better reporting.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): fixed reporting.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): add complete report and feature for benchmark.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): change report for file writing, to be easier to accumulate better.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): enhance report and extraction of stats during tests.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): fix typo
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): fix open type
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): fix open type
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): fix report file creation
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): fix report file creation
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): fixed, time was in seconds
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): fixed report and monitor.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(sinker): enhance short-report, so there is no need to iterate over full statistics.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): add profile e2e tests, and wip on other tests.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): add profile test for e2e agent
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): add profile test for calling start
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(test): skip bench tests in CICD.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): fix high CPU usage.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    * feat(agent): fix docker container not stopping.
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>
    
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

---
+++ python-test/features/steps/benchmark.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+import threading
+import docker
+from behave import then, step
+import json
+
+MINUTE_INT = 60
+client = docker.from_env()
+
+
+@step("agent will be monitored in cpu and memory usage with {policy_amount} policies applied")
+def prepare_monitor(context,policy_amount):
+    context.policy_amount = policy_amount
+
+
+@step("monitor the activity of memory usage during {monitor_time} minutes")
+def monitor_docker_stats_during(context, monitor_time):
+    started = datetime.now().timestamp()
+    monitored_duration = 0
+    event = threading.Event()
+    sr_file_name = "short-report-" + context.policy_amount +"policies-" + str(started) + ".json"
+    short_report_file = open(sr_file_name, "x")
+    short_report = [{}]
+    lr_file_name = "docker-container-stats-" + context.policy_amount +"policies-" + str(started) + ".json"
+    long_report_file = open(lr_file_name, "x")
+    long_report = [{}]
+    monitor_limit = int(monitor_time) * int(MINUTE_INT)
+    while not event.is_set() and monitored_duration <= monitor_limit:
+        container = client.containers.get(context.container_id)
+        print("Monitor docker stats for", context.policy_amount, "policies")
+        statistics = container.stats(stream=False)
+        used_memory = int(statistics["memory_stats"]["usage"]) - int(statistics["memory_stats"]["stats"]["file"])
+        available_memory = int(statistics["memory_stats"]["limit"])
+        cpu_delta = int(statistics["cpu_stats"]["cpu_usage"]["total_usage"]) - int(statistics["precpu_stats"]["cpu_usage"]["total_usage"])
+        system_cpu_delta = int(statistics["cpu_stats"]["system_cpu_usage"]) - int(statistics["precpu_stats"]["system_cpu_usage"])
+        number_cpus = int(statistics["cpu_stats"]["online_cpus"])
+        cpu_percent = (cpu_delta / system_cpu_delta) * number_cpus * 100.0
+        memory_percent = (used_memory / available_memory) * 100.0
+        # print to a file instead of printing in console
+        print("Memory Usage", memory_percent, " %")
+        print("CPU Usage ", cpu_percent)
+        short_report.append({"timestamp": datetime.now().timestamp(), "cpu_percent": cpu_percent, "memory_usage": memory_percent})
+        long_report.append(statistics)
+        event.wait(30)
+        monitored_duration = datetime.now().timestamp() - started
+        print("monitored duration", monitored_duration, "seconds")
+        print("waiting for ", monitor_limit)
+    json.dump(short_report, short_report_file)
+    json.dump(long_report, long_report_file)
