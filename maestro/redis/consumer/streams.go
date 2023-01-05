43d9640cd2375a3330f9649223cf3c9416eec473
Luiz Pegoraro <luiz.pegoraro@encora.com>
2023-01-05 19:21:01 -0300

    feat(maestro): fix cycle dependency.
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- maestro/redis/consumer/streams.go
+++ maestro/redis/consumer/streams.go
@@ -120,12 +120,12 @@ func (es eventStore) SubscribeSinks(context context.Context) error {
 			}
 			switch event["operation"] {
 			case sinksCreate:
-				if v, ok := rte.config["opentelemetry"]; ok && v.(string) == "enabled" {
+				if v, ok := rte.Config["opentelemetry"]; ok && v.(string) == "enabled" {
 					err = es.handleSinksCreateCollector(context, rte) //should create collector
 				}
 
 			case sinksUpdate:
-				if v, ok := rte.config["opentelemetry"]; ok && v.(string) == "enabled" {
+				if v, ok := rte.Config["opentelemetry"]; ok && v.(string) == "enabled" {
 					err = es.handleSinksUpdateCollector(context, rte) //should create collector
 				}
 
