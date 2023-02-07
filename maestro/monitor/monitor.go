40ef800f9b92487860b6913f2117d4bcb9b0416a
Luiz Pegoraro <luiz.pegoraro@encora.com>
2023-02-07 11:57:10 -0500

    fix(maestro): warning as error.
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- maestro/monitor/monitor.go
+++ maestro/monitor/monitor.go
@@ -254,7 +254,7 @@ func (svc *monitorService) analyzeLogs(logEntry []string) (status string, err er
 			}
 			if strings.Contains(logLine, "Permanent error: remote write returned HTTP status 429 Too Many Requests") {
 				errorMessage := "Permanent error: remote write returned HTTP status 429 Too Many Requests"
-				return "warning", errors.New(errorMessage)
+				return "error", errors.New(errorMessage)
 			}
 			// other errors
 			if strings.Contains(logLine, "error") {
