0f9e7e62bfa83812bcc76287952aaa2a1b9f01b8
Luiz Pegoraro <luiz.pegoraro@encora.com>
2023-02-10 09:12:05 -0500

    fix(maestro): clean up code and enhanced verification of collector apply.
    Signed-off-by: Luiz Pegoraro <luiz.pegoraro@encora.com>

--- maestro/monitor/monitor.go
+++ maestro/monitor/monitor.go
@@ -170,13 +170,7 @@ func (svc *monitorService) monitorSinks(ctx context.Context) {
 		if sink == nil {
 			svc.logger.Warn("collector not found for sink, depleting collector", zap.String("collector name", collector.Name))
 			sinkId := collector.Name[5:51]
-			svc.logger.Info("Debug extracted sinkID", zap.String("sinkID", sinkId))
-			deployment, errDeploy := svc.eventStore.GetDeploymentEntryFromSinkId(ctx, sinkId)
-			if errDeploy != nil {
-				svc.logger.Error("Remove collector: error on getting collector deployment from redis", zap.Error(errDeploy))
-				continue
-			}
-			err = svc.kubecontrol.DeleteOtelCollector(ctx, sink.OwnerID, sink.Id, deployment)
+			err = svc.kubecontrol.DeleteOtelCollector(ctx, "", sinkId, "")
 			if err != nil {
 				svc.logger.Error("error removing otel collector", zap.Error(err))
 			}
