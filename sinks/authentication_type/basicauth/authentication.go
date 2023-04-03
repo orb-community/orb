ccd13bc88ae067e62150cc370121ad9decb8bfa2
Luiz Pegoraro <lpegoraro@netboxlabs.com>
2023-04-03 15:58:33 -0300

    feat(sinks): add validate config for basic auth.

--- sinks/authentication_type/basicauth/authentication.go
+++ sinks/authentication_type/basicauth/authentication.go
@@ -1,8 +1,10 @@
 package basicauth
 
 import (
+	"github.com/orb-community/orb/pkg/errors"
 	"github.com/orb-community/orb/sinks/authentication_type"
 	"github.com/orb-community/orb/sinks/backend"
+	"gopkg.in/yaml.v3"
 )
 
 const (
@@ -10,42 +12,71 @@ const (
 	PasswordConfigFeature = "password"
 )
 
+var (
+	features = []authentication_type.ConfigFeature{
+		{
+			Type:     backend.ConfigFeatureTypeText,
+			Input:    "text",
+			Title:    "Username",
+			Name:     UsernameConfigFeature,
+			Required: true,
+		},
+		{
+			Type:     backend.ConfigFeatureTypePassword,
+			Input:    "text",
+			Title:    "Password",
+			Name:     PasswordConfigFeature,
+			Required: true,
+		},
+	}
+)
+
 type AuthConfig struct {
-	username string `json:"username", yaml:"username"`
-	password string `json:"password", yaml:"password"`
+	Username          string `json:"username" ,yaml:"username"`
+	Password          string `json:"password" ,yaml:"password"`
+	encryptionService authentication_type.PasswordService
 }
 
 func (a *AuthConfig) Metadata() interface{} {
+
 	return authentication_type.AuthenticationTypeConfig{
 		Type:        "basicauth",
 		Description: "Basic username and password authentication",
-		Config: []authentication_type.ConfigFeature{
-			{
-				Type:     backend.ConfigFeatureTypeText,
-				Input:    "text",
-				Title:    "Username",
-				Name:     UsernameConfigFeature,
-				Required: true,
-			},
-			{
-				Type:     backend.ConfigFeatureTypePassword,
-				Input:    "text",
-				Title:    "Password",
-				Name:     PasswordConfigFeature,
-				Required: true,
-			},
-		},
+		Config:      features,
 	}
 }
 
 func (a *AuthConfig) GetFeatureConfig() []authentication_type.ConfigFeature {
-	//TODO implement me
-	panic("implement me")
+	return features
 }
 
 func (a *AuthConfig) ValidateConfiguration(inputFormat string, input interface{}) error {
-	//TODO implement me
-	panic("implement me")
+	switch inputFormat {
+	case "object":
+		for key, value := range input.(map[string]string) {
+			if key == UsernameConfigFeature {
+				if len(value) == 0 {
+					return errors.New("username cannot be empty")
+				}
+			}
+			if key == PasswordConfigFeature {
+				if len(value) == 0 {
+					return errors.New("password cannot be empty")
+				}
+			}
+		}
+	case "yaml":
+		err := yaml.Unmarshal([]byte(input.(string)), &a)
+		if err != nil {
+			return err
+		}
+		if len(a.Username) == 0 {
+			return errors.New("username cannot be empty")
+		} else if len(a.Password) == 0 {
+			return errors.New("password cannot be empty")
+		}
+	}
+	return nil
 }
 
 func (a *AuthConfig) ConfigToFormat(outputFormat string, input interface{}) (interface{}, error) {
