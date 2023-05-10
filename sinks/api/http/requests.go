553a96a4428532fcb26bcd20c2c2dc28bdc7a6bd
Bruno Valença <brunovalenca10@gmail.com>
2023-05-10 12:03:49 -0300

    garbage removal

--- sinks/api/http/requests.go
+++ sinks/api/http/requests.go
@@ -9,8 +9,6 @@
 package http
 
 import (
-	"fmt"
-
 	"github.com/orb-community/orb/pkg/errors"
 	"github.com/orb-community/orb/pkg/types"
 	"github.com/orb-community/orb/sinks"
@@ -104,7 +102,6 @@ func (req updateSinkReq) validate(sinkBackend backend.Backend) error {
 		if req.Format == "yaml" {
 			config, err = sinkBackend.ParseConfig(req.Format, req.ConfigData)
 			if err != nil {
-				fmt.Println("Passou pela verificação no format")
 				return errors.Wrap(errors.ErrMalformedEntity, err)
 			}
 		} else {
