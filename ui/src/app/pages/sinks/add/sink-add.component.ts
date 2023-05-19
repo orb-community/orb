38d75841c0f10dbaecd9f9e8e4761e5822d4e27f
joaoguilherme2003 <joao.mendonca@encora.com>
2023-05-19 17:41:22 -0300

    feat(orb-ui): #777 New page for sink creation (#2378)

---
+++ ui/src/app/pages/sinks/add/sink-add.component.ts
@@ -0,0 +1,72 @@
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Sink } from 'app/common/interfaces/orb/sink.interface';
+import { NotificationsService } from 'app/common/services/notifications/notifications.service';
+import { SinksService } from 'app/common/services/sinks/sinks.service';
+import { SinkConfigComponent } from 'app/shared/components/orb/sink/sink-config/sink-config.component';
+import { SinkDetailsComponent } from 'app/shared/components/orb/sink/sink-details/sink-details.component';
+import { STRINGS } from 'assets/text/strings';
+
+
+@Component({
+    selector: 'ngx-sink-add-component',
+    templateUrl: './sink-add.component.html',
+    styleUrls: ['./sink-add.component.scss'],
+})
+
+export class SinkAddComponent {
+
+    @ViewChild(SinkDetailsComponent) detailsComponent: SinkDetailsComponent;
+
+    @ViewChild(SinkConfigComponent) configComponent: SinkConfigComponent;
+
+    strings = STRINGS;
+
+    createMode: boolean;
+
+    constructor(
+        private sinksService: SinksService,
+        private notificationsService: NotificationsService,
+        private router: Router,
+    ) {
+        this.createMode = true;
+    }
+
+    canCreate() { 
+        const detailsValid = this.createMode
+        ? this.detailsComponent?.formGroup?.status === 'VALID'
+        : true;
+        return detailsValid;
+    }
+
+    createSink() {
+
+        const sinkDetails = this.detailsComponent.formGroup?.value;
+        const tags = this.detailsComponent.selectedTags;
+        const configSink = this.configComponent.code;
+
+        const details = { ...sinkDetails};
+
+        let configs = JSON.parse(configSink);
+
+        const config = {
+            ...configs
+        }
+
+        const payload = {
+            ...details,
+            tags,
+            config,
+        
+        } as Sink;
+
+        this.sinksService.addSink(payload).subscribe(() => {
+            this.notificationsService.success('Sink successfully created', '');
+            this.goBack();
+        });
+    }
+
+    goBack() {
+        this.router.navigateByUrl('/pages/sinks');
+    }
+}
