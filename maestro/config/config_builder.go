c126b4882705478fc90bbd794d7048771335a336
etaques <etaques@daitan.com>
2023-06-24 18:22:58 -0300

    add header in maestro

--- maestro/config/config_builder.go
+++ maestro/config/config_builder.go
@@ -391,8 +391,31 @@ func ReturnConfigYamlFromSink(ctx context.Context, kafkaURL string, sink SinkDat
   extensions.PProf = &PProfExtension{
       Endpoint: "0.0.0.0:1888",
   }
+  var extensionNames []string
+
+  // Check to add X-Scope-OrgID header
+  header := sink.Config.GetSubMetadata("headers")["X-Scope-OrgID"]
+  headerStr, ok := header.(string)
+  if !ok {
+    extensionNames = []string{"pprof", extensionName}
+  } else {
+    extensionNames = []string{"pprof", extensionName, "headers_setter"}
+
+    headerConfig := HeaderConfig{
+      Action: "insert",
+      Key: "X-Scope-OrgID",
+      FromContext: headerStr,
+    }
+
+    extensions.HeadersSetter = &HeadersSetterExtension{
+      Headers: []HeaderConfig{
+        headerConfig,
+      },
+    }
+  }
+  
   serviceConfig := ServiceConfig{
-      Extensions: []string{"pprof", extensionName},
+      Extensions: extensionNames,
       Pipelines: struct {
           Metrics struct {
               Receivers  []string `json:"receivers" yaml:"receivers"`
