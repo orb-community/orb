9555f65866cf98e634190c40d96663ced861af8a
Luiz Pegoraro <lpegoraro@netboxlabs.com>
2023-07-14 14:34:34 -0300

    fix(agent): fix context cancellation.

--- agent/otel/bridgeservice.go
+++ agent/otel/bridgeservice.go
@@ -21,13 +21,15 @@ var _ AgentBridgeService = (*BridgeService)(nil)
 
 type BridgeService struct {
 	bridgeContext context.Context
+	cancelFunc    context.CancelFunc
 	policyRepo    policies.PolicyRepo
 	AgentTags     map[string]string
 }
 
-func NewBridgeService(ctx context.Context, policyRepo *policies.PolicyRepo, agentTags map[string]string) *BridgeService {
+func NewBridgeService(ctx context.Context, cancelFunc context.CancelFunc, policyRepo *policies.PolicyRepo, agentTags map[string]string) *BridgeService {
 	return &BridgeService{
 		bridgeContext: ctx,
+		cancelFunc:    cancelFunc,
 		policyRepo:    *policyRepo,
 		AgentTags:     agentTags,
 	}
@@ -47,5 +49,5 @@ func (b *BridgeService) RetrieveAgentInfoByPolicyName(policyName string) (*Agent
 
 func (b *BridgeService) NotifyAgentDisconnection(ctx context.Context, err error) {
 	ctx.Done()
-	b.bridgeContext.Done()
+	b.cancelFunc()
 }
