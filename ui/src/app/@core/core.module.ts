6d752712d1e08ca33cb5d188841e0781f704e801
Guilhermo Pazuch <1490938+gpazuch@users.noreply.github.com>
2023-08-28 12:03:14 -0300

    fix(eng-13): fix no auth strategy name

--- ui/src/app/@core/core.module.ts
+++ ui/src/app/@core/core.module.ts
@@ -47,6 +47,10 @@ export const NB_CORE_PROVIDERS = [
             resetPass: {
               endpoint: environment.resetPassUrl,
               method: 'put',
+              redirect: {
+                success: '/auth/login',
+                failure: null
+              }
             },
             logout:
              { method: null, redirect: { success: '/', failure: '/' } },
