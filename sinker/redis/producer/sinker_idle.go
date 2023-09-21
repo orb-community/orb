320c14a771f9476b5963dc6f766ef4016a501076
Luiz Pegoraro <pegorarohluiz@gmail.comw>
2023-09-21 11:18:24 -0300

    feat(sinker): WIP.

--- sinker/redis/producer/sinker_idle.go
+++ sinker/redis/producer/sinker_idle.go
@@ -3,6 +3,7 @@ package producer
 import (
 	"context"
 	"github.com/go-redis/redis/v8"
+	"go.uber.org/zap"
 	"time"
 )
 
@@ -34,4 +35,19 @@ var _ SinkIdleProducer = (*sinkIdleProducer)(nil)
 type sinkIdleProducer struct {
 	logger            *zap.Logger
 	redisStreamClient redis.Client
+}
+
+func (s *sinkIdleProducer) PublishSinkIdle(ctx context.Context, event SinkIdleEvent) error {
+	const maxLen = 1000
+	record := &redis.XAddArgs{
+		Stream: "orb.sinker.sink_idle",
+		Values: event.Encode(),
+		MaxLen: maxLen,
+		Approx: true,
+	}
+	err := s.redisStreamClient.XAdd(ctx, record).Err()
+	if err != nil {
+		s.logger.Error("error sending event to sinker event store", zap.Error(err))
+	}
+	return err
 }
