a0bc1a6203d1513834e71fa6d5362548dfca4b64
Luiz Pegoraro <pegorarohluiz@gmail.comw>
2023-11-20 19:43:48 -0300

    fix(agent): turn off telemetry by default.

--- agent/backend/otel/exporter_builder.go
+++ agent/backend/otel/exporter_builder.go
@@ -35,6 +35,7 @@ type service struct {
 
 type telemetry struct {
 	Metrics *metrics `yaml:"metrics"`
+	Enabled bool     `yaml:"enabled,omitempty"`
 }
 
 type metrics struct {
@@ -72,7 +73,7 @@ func (e *exporterBuilder) GetStructFromYaml(yamlString string) (openTelemetryCon
 	return config, nil
 }
 
-func (e *exporterBuilder) MergeDefaultValueWithPolicy(config openTelemetryConfig, policyId string, policyName string, telemetryPort int) (openTelemetryConfig, error) {
+func (e *exporterBuilder) MergeDefaultValueWithPolicy(config openTelemetryConfig, policyId string, policyName string, _ int) (openTelemetryConfig, error) {
 	endpoint := e.host + ":" + strconv.Itoa(e.port)
 	defaultOtlpExporter := defaultOtlpExporter{
 		Endpoint: endpoint,
@@ -101,7 +102,7 @@ func (e *exporterBuilder) MergeDefaultValueWithPolicy(config openTelemetryConfig
 		config.Extensions = make(map[string]interface{})
 	}
 	tel := &telemetry{
-		Metrics: &metrics{Address: "0.0.0.0:" + strconv.Itoa(telemetryPort)},
+		Enabled: false,
 	}
 	config.Service.Telemetry = tel
 	// Override metrics exporter and append attributes/policy_data processor
