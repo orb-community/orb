4210d16fe4010c6313b0123fd3efd17922136f98
Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>
2023-11-27 12:31:06 +0000

    fix(agent): default backend variables
    Signed-off-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

--- agent/backend/otel/vars.go
+++ agent/backend/otel/vars.go
@@ -2,6 +2,6 @@ package otel
 
 import "github.com/spf13/viper"
 
-func RegisterBackendSpecificVariables(viper *viper.Viper) {
+func RegisterBackendSpecificVariables() {
 	viper.SetDefault("orb.backends.otel.otlp_port", "4316")
 }
