204e13c64ed35131be67f615128645e648f90266
Luiz Pegoraro <lpegoraro@netboxlabs.com>
2023-12-08 13:26:42 -0300

    feat(sinks): fix noauth authentication passthrough.

--- sinks/authentication_type/noauth/authentication.go
+++ sinks/authentication_type/noauth/authentication.go
@@ -1,6 +1,11 @@
 package noauth
 
-import "github.com/orb-community/orb/sinks/authentication_type"
+import (
+	"github.com/orb-community/orb/pkg/errors"
+	"github.com/orb-community/orb/pkg/types"
+	"github.com/orb-community/orb/sinks/authentication_type"
+	"gopkg.in/yaml.v3"
+)
 
 var features = []authentication_type.ConfigFeature{}
 
@@ -16,20 +21,40 @@ func (a *AuthConfig) ValidateConfiguration(_ string, _ interface{}) error {
 	return nil
 }
 
-func (a *AuthConfig) ConfigToFormat(_ string, _ interface{}) (interface{}, error) {
-	return nil, nil
+func (a *AuthConfig) ConfigToFormat(outputFormat string, input interface{}) (interface{}, error) {
+	switch input.(type) {
+	case types.Metadata:
+		if outputFormat == "yaml" {
+			retVal, err := yaml.Marshal(input)
+			return string(retVal), err
+		}
+	case string:
+		if outputFormat == "object" {
+			retVal := make(types.Metadata)
+			val := input.(string)
+			err := yaml.Unmarshal([]byte(val), &retVal)
+			return retVal, err
+		} else {
+			return nil, errors.New("unsupported format")
+		}
+	}
+	return nil, errors.New("unsupported format")
 }
 
-func (a *AuthConfig) OmitInformation(_ string, _ interface{}) (interface{}, error) {
-	return nil, nil
+// OmitInformation just pass-through the config without changes
+func (a *AuthConfig) OmitInformation(_ string, c interface{}) (interface{}, error) {
+	return c, nil
 }
 
-func (a *AuthConfig) EncodeInformation(_ string, _ interface{}) (interface{}, error) {
-	return nil, nil
+// EncodeInformation just pass-through the config without changes
+func (a *AuthConfig) EncodeInformation(_ string, c interface{}) (interface{}, error) {
+
+	return c, nil
 }
 
-func (a *AuthConfig) DecodeInformation(_ string, _ interface{}) (interface{}, error) {
-	return nil, nil
+// DecodeInformation just pass-through the config without changes
+func (a *AuthConfig) DecodeInformation(_ string, c interface{}) (interface{}, error) {
+	return c, nil
 }
 
 func (a *AuthConfig) Metadata() authentication_type.AuthenticationTypeConfig {
